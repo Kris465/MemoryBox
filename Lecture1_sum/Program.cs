648ca4fb821ea3c66ad14de1c0b89b2911b0a254
Tasha <nataza7394@gmail.com>
2022-08-19 20:05:58 +0300

    Lecture 1. C#. Sum

---
+++ Lecture1_sum/Program.cs
@@ -0,0 +1,10 @@
+﻿/*
+int number1 = 3;
+int number2 = 5;
+Console.WriteLine(number1 + number2);
+*/
+
+int number1 = 3;
+int number2 = 5;
+int result = number1 + number2; // We have created a new box for the sum of two numbers, especially we will need it
+Console.WriteLine(result);
