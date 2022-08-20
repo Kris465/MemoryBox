b2f87a33a0b9e8c77160a4894597b2b9785b7ed2
Tasha <nataza7394@gmail.com>
2022-08-20 14:33:27 +0300

    Information what was got in workshop.

---
+++ Workshop1_lesson/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine("The square is " + square);
+*/
+
+/*Console.Write("Input number 1: ");
+Console.Write("Input number 2: ");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int square = number2 * number2;
+
+if (number1 == square)
+{
+    Console.Write("The first number is a square of second number");
+}
+else 
+{
+    Console.Write("The second number isnt a square");
+}
+*/
+
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = number * (-1);
+
+while(current <= number)
+{
+    Console.Write(current + " ");
+    current ++;
+    
+}
+
