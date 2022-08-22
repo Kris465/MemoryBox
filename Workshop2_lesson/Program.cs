9bf167dc2398a6e6f6182ec9afa95e3b1772c5ff
Tasha <nataza7394@gmail.com>
2022-08-22 11:45:08 +0300

    Workshop2 - working script

--- Workshop2_lesson/Program.cs
+++ Workshop2_lesson/Program.cs
@@ -21,21 +21,23 @@ Console.WriteLine($"Bigger digit of {randNumber} is {biggerDigit}");
 
 // Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
 
-/*
+
 int RemoveSecondDigit(int number)
 {
     int digit1 = number / 100;
     int digit3 = number % 10;
-    return digit1 * 10 + digit3;
+    int result = digit1 * 10 + digit3;
+    return result;
 }
 
-int randNumber = new Random().Next(minValue: 100. maxValue: 1000);
-int result = RemoveSecondDigit(randNumber);
-Console.WriteLine($"Число без второй цифры числа {randNumber} это {result}");
-*/
+int randNumber = new Random().Next(100, 1000);
+int completeNumber = RemoveSecondDigit(randNumber);
+Console.Write($"Cut version of {randNumber} is {completeNumber}.");
+
 
 // Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если второе число не кратно числу первому, то программа выводит остаток от деления.
 
+/*
 void CheckMultiplier(int number1, int number2)
 {
     int rest = number2 % number1;
@@ -58,3 +60,4 @@ Console.Write("Введите второе число ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
 CheckMultiplier(num1, num2);
+*/
