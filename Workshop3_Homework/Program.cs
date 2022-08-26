8c14391329bde17e6daf58a327cd0e24ef9d62eb
Tasha <nataza7394@gmail.com>
2022-08-26 21:31:18 +0300

    It is out of order, but almost finished

--- Workshop3_Homework/Program.cs
+++ Workshop3_Homework/Program.cs
@@ -86,6 +86,7 @@ int num = Convert.ToInt32(Console.ReadLine());
 Palindrome(num);
 */
 
+/*
 void Palindrome(int number)
 {
      int length = 1;
@@ -112,14 +113,73 @@ void Palindrome(int number)
                cutnum = cutnum / 10;
           }
           check = check + 1;
+          break;
      } 
 }
 
 Console.Write("Input yout number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 Palindrome(num);
+*/
+
+/*
+void Palindrome(int number)
+{
+     int cutnum;
+     int inverse = 0;
+     while(number > 0)
+     {
+          cutnum = number % 10;
+          inverse = inverse * 10 + cutnum;
+          number = number / 10;
+     }
+
+     if(number == inverse)
+     Console.WriteLine($"Your number is a palindrome {number} {inverse}");
+     else
+     Console.WriteLine($"Your number isn't a palindrome {number} {inverse}");
+}
+
+Console.Write("Input yout number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Palindrome(num);
+*/
 
 // 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
-// 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+double FindDistance(double x1, double y1, double z1, double x2, double y2, , double z2);
+{
+    return Math.Sqrt((x2 - x1) * (x2 - x1) * (y2 - y1) * (y2 - y1) * (z2-z1) * (z2-z1));
+}
 
+Console.WriteLine("Enter : ");
+double xA = Convert.ToDouble(Console.ReadLine());
+double yA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter the second dot: ");
+double xB = Convert.ToDouble(Console.ReadLine());
+double yB = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter the second dot: ");
+double zA = Convert.ToDouble(Console.ReadLine());
+double zB = Convert.ToDouble(Console.ReadLine());
+
+double dist = FindDistance(xA, yA, xB, yB, zA, zB);
+Console.WriteLine($"Distance is {dist}");
+
+// 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+/*
+void Cube(int number)
+{
+    int current = 1;
+    while(current <= number)
+    {
+        Console.Write(Math.Pow(current, 3) + " ");
+        current ++;
+    }
+}
+
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Cube(num);
+*/
