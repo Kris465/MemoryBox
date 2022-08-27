64ba605fe7b53033b9ac14daec5663434fec1614
Tasha <nataza7394@gmail.com>
2022-08-27 11:32:39 +0300

    Working homework

--- Workshop3_Homework/Program.cs
+++ Workshop3_Homework/Program.cs
@@ -122,50 +122,48 @@ int num = Convert.ToInt32(Console.ReadLine());
 Palindrome(num);
 */
 
-/*
+
 void Palindrome(int number)
 {
-     int cutnum;
      int inverse = 0;
+     int newnum = number;
      while(number > 0)
      {
-          cutnum = number % 10;
-          inverse = inverse * 10 + cutnum;
+          int remind = number % 10;
+          inverse = inverse * 10 + remind;
           number = number / 10;
      }
 
-     if(number == inverse)
-     Console.WriteLine($"Your number is a palindrome {number} {inverse}");
-     else
-     Console.WriteLine($"Your number isn't a palindrome {number} {inverse}");
+     if(newnum == inverse) Console.WriteLine($"Your number is a palindrome {inverse}");
+     else Console.WriteLine($"Your number isn't a palindrome {inverse}");
 }
 
 Console.Write("Input yout number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 Palindrome(num);
-*/
+
 
 // 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
-double FindDistance(double x1, double y1, double z1, double x2, double y2, , double z2);
+/*
+double FindDistance(double x1, double y1, double z1, double x2, double y2, double z2)
 {
-    return Math.Sqrt((x2 - x1) * (x2 - x1) * (y2 - y1) * (y2 - y1) * (z2-z1) * (z2-z1));
+    return Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2-z1) * (z2-z1));
 }
 
-Console.WriteLine("Enter : ");
+Console.WriteLine("Enter the first dot (x, y, z) on their own lines: ");
 double xA = Convert.ToDouble(Console.ReadLine());
 double yA = Convert.ToDouble(Console.ReadLine());
+double zA = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Enter the second dot: ");
+Console.WriteLine("Enter the second dot (x, y, z) on their own lines: ");
 double xB = Convert.ToDouble(Console.ReadLine());
 double yB = Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("Enter the second dot: ");
-double zA = Convert.ToDouble(Console.ReadLine());
 double zB = Convert.ToDouble(Console.ReadLine());
 
-double dist = FindDistance(xA, yA, xB, yB, zA, zB);
+double dist = FindDistance(xA, yA, zA, xB, yB, zB);
 Console.WriteLine($"Distance is {dist}");
+*/
 
 // 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 /*
