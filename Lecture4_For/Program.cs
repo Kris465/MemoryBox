adbc7aa187a5f3a4a8d8517f507b9c308005ec80
Tasha <nataza7394@gmail.com>
2022-08-29 00:37:20 +0300

    for

---
+++ Lecture4_For/Program.cs
@@ -0,0 +1,17 @@
+﻿// For
+/*
+string Method4(int count, string c)
+{
+    string result = String.Empty;
+    
+    for (int i = 0; i < count; i ++) // Инициализация счетчика, проверка условия, инкремент
+    {
+        result = result + c;
+    }
+    return result;
+}
+
+string res = Method4(10, "asdfa");
+Console.WriteLine(res);
+*/
+
