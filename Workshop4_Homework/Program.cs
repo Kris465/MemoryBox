f20ff60e8212ab0490c5caa271c0850f29916e99
Tasha <nataza7394@gmail.com>
2022-08-29 22:12:07 +0300

    One more try

--- Workshop4_Homework/Program.cs
+++ Workshop4_Homework/Program.cs
@@ -42,4 +42,67 @@ Console.WriteLine(SumOfDigits(num));
 */
 
 // 29. Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+/*
+int[] AskArray(int size, int[] num)
+{
+    int[] newArray = new int[size];
 
+    for(int i = 0; i < size; i++) 
+        newArray[i] = Convert.ToInt32(Console.ReadLine());
+
+    return newArray;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i ++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();    
+}
+
+Console.WriteLine("Input the size of your array: ");
+int askedsize = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input numbers for your array: ");
+int[] num = Convert.ToInt32(Console.ReadLine());
+ShowArray(AskArray(askedsize, num));
+*/
+
+/* // Кусок некой херни
+void Main(string[] args)
+    {
+        Console.WriteLine("Введите размерность массива:");
+        int i, n = Convert.ToInt32(Console.ReadLine());
+        int[] mas = new int[n];
+        Console.WriteLine("Введите массив:");
+        string[] str = Console.ReadLine().Split(new char[] { ' ', '\n', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+        for (i = 0; i < (n < str.Length ? n : str.Length); ++i)
+            mas[i] = Convert.ToInt32(str[i]);
+        Console.WriteLine("Введенный массив:");
+        for (i = 0; i < n; ++i)
+            Console.Write("{0} ", mas[i]);
+        Console.ReadLine();
+    }
+
+*/
+
+int[] CreateRandomArray(int size, int minValue, int maxValue) 
+{
+    int[] newArray = new int[size];
+
+    for(int i = 0; i < size; i++) 
+        newArray[i] = new Random().Next(minValue, maxValue + 1);
+
+    return newArray;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i ++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();    
+}
+
+ShowArray(CreateRandomArray(25, 0, 9));
