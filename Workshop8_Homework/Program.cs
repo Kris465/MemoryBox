86b0502e271cef6ae614ceddb54d9fbf320519fc
Tasha <nataza7394@gmail.com>
2022-09-15 11:01:45 +0300

    lesson9

--- Workshop8_Homework/Program.cs
+++ Workshop8_Homework/Program.cs
@@ -33,14 +33,82 @@ void ShowArray2(int[,] array)
         
         Console.WriteLine();
     }
+    Console.WriteLine();
 }
 
+/*
+int[,] SortedArray(int[,] array)
+{
+    int[,] newArray = array;
 
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            for (int line = 0; line > i; line++)
+            {
+                if (array[line, j] < array[i,j])
+                {
+                    int temp = array[line,j];
+                    array[line,j] = array[i, j];
+                    array[i, j] = temp;
+                }
+            }
+        }
+    }
+    return newArray;
+}
+*/
 
+/*
+int j = 0;
 
+int SortedArray(int[,] array, int column)
+{
+    int[,] newArray = array;
+    int max = array[0,0];
 
+    for (int i = 0; i < newArray.GetLength(0); i++)
+    {
+        newArray[i,j] > newArray[i + 1, j];
+        int temp = array[line,j];
+        array[line,j] = array[i, j];
+        array[i, j] = temp;
+    }
+    return j;
+}
+
+*/
+
+int[,] SortedArray(int[,] array)
+{
+    int[,] newArray = new int[array.GetLength(0),array.GetLength(1)];
+    int max = 0;
+    int current = 0;
+
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == current)
+            {
+                if (array[i, j] < array[i, j + 1])
+                {
+                    int temp = array[i,j]
+                } 
+            }
+        }
+            
+        
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    return newArray[,];
+}
 
 int[,] myArray = CreateRandom2Array();
+ShowArray2(myArray);
+SortedArray(myArray, j);
 ShowArray2(myArray);
 
 
---
+++ Workshop_lesson9/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+/*
+void ShowNums(int n)
+{
+    if(n > 1) ShowNums(n-1);
+
+    Console.Write(n + " ");
+}
+
+ShowNums(5);
+*/
+// Условие работы? 
+
+// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+/*
+int SumOfDigits(int n)
+{
+    if (n > 0) return SumOfDigits(n / 10) + n % 10;
+    else return 0;
+}
+
+Console.Write(SumOfDigits(5364));
+
+// 5364
+*/
+
+// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+/*
+void ShowNums(int m, int n)
+{
+    if(m > n) ShowNums(m, n-1);
+    Console.Write(n + " ");
+}
+
+ShowNums(29, 1);
+*/
+// Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии. 
+
+int Degree(int a, int b)
+{
+    if (b > 1) return Degree(a, b - 1)*a;
+    else return a;
+}
+
+Console.WriteLine(Degree(2, 3));
