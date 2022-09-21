578fd851ca673e04957d620d46aa44d22d2be68a
Tasha <nataza7394@gmail.com>
2022-09-21 15:40:13 +0300

    Done homework

--- Workshop9_Homework/Program.cs
+++ Workshop9_Homework/Program.cs
@@ -32,12 +32,14 @@ Console.Write(SumOfDigits(min, num));
 
 int FuncAkkerman(int n, int m)
 {
-    if(n != 0 && m != 0) return FuncAkkerman(n - 1, FuncAkkerman(n, m - 1));
+    if(n == 0) return m + 1;
+    else if(n > 0 && m == 0) return FuncAkkerman(n - 1, 1);
+    else if(n > 0 && m > 0) return FuncAkkerman(n - 1, FuncAkkerman(n, m - 1));
     else return 0;
 }
 
 Console.WriteLine("Input the first numer: ");
-int min = Convert.ToInt32(Console.ReadLine()); 
+int first = Convert.ToInt32(Console.ReadLine()); 
 Console.WriteLine("Input the first numer: ");
-int num = Convert.ToInt32(Console.ReadLine()); 
-Console.Write(FuncAkkerman(min, num));
+int second = Convert.ToInt32(Console.ReadLine()); 
+Console.Write(FuncAkkerman(second, first));
