422f2a524bf6d7adeccbd9d314aac31433979491
Tasha <nataza7394@gmail.com>
2022-10-14 00:16:45 +0300

    task4 and task5

--- Workshop1_lesson/task3.py
+++ Workshop1_lesson/task3.py
@@ -1,6 +1,7 @@
 # 2. Найти максимальное из пяти чисел
 
 lst = []
+# lst = [int(i) for i in input().split()]
 for i in range(5):
     lst.append(int(input("Input number: ")))
 
@@ -8,6 +9,8 @@ max_ = 0
 for elem in lst:
     if elem > max_:
         max_ = elem
+
+# print(max(lst))
 
 print(lst)
 print(max_)
