781bc690ccefd7b74ac4877b1cf7d9dc35f302c2
Tasha <nataza7394@gmail.com>
2022-10-27 16:14:53 +0300

    second task is done

--- Workshop2_homework/task2.py
+++ Workshop2_homework/task2.py
@@ -3,3 +3,15 @@
 # Пример:
 
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+def set_of_multi():
+    n = int(input("Input your number, please: "))
+    ls = []
+    result = 1
+
+    for i in range(1, n + 1):
+        result *= i
+        ls.append(result)
+    print(f"Yout list is: {ls}") 
+
+set_of_multi()
