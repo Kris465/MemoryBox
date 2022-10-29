74ff6193853bee081fc6148d8ced17ab7a29545f
Tasha <nataza7394@gmail.com>
2022-10-29 19:32:12 +0300

    fourth task has been done

--- Workshop2_homework/task5.py
+++ Workshop2_homework/task5.py
@@ -1,1 +1,33 @@
 # Реализуйте алгоритм перемешивания списка.
+
+import random
+
+
+def input_num():
+    n = int(input("Введите длину списка: "))
+    return n  
+        
+
+def create_random_lst(size):
+    lst = []
+    iMin = int(input("Введите минимальное рандомное число: "))
+    iMax = int(input("Введите максимальное рандомное число: "))
+  
+    for i in range(size):
+        lst.append(random.randint(iMin, iMax))
+    return lst
+
+
+def shuffle_lst(lst):
+    for i in range(len(lst)):
+        rnd = random.randint(0, len(lst))
+        lst[i], lst[rnd] = lst[rnd], lst[i]
+        return lst
+        
+    
+
+
+my_list = create_random_lst(input_num())
+print(f'Рандомный список выглядит так: {my_list}')
+
+print(f'Перемешанный список выглядит так: {shuffle_lst(my_list)}')
