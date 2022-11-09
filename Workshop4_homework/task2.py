bc0f453428f6a4391e743cc6e49253f9f70f09d3
Tasha <nataza7394@gmail.com>
2022-11-09 21:39:58 +0300

    second task has remade

--- Workshop4_homework/task2.py
+++ Workshop4_homework/task2.py
@@ -1,28 +1,17 @@
 # Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 
 def simple_multipliers():
-    number = int(input("Input your number, please: "))
-    simple_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89,
-                        97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 
-                        191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281,
-                        283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397,
-                        401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503,
-                        509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 
-                        631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 
-                        751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863,
-                        877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
+    number = int(input("Input yout number, please: "))
+    multipliers = []
+    a = 2
 
-    i = 0
-    simple_multi = []
+    while number > 1:
+        if number % a == 0:
+            multipliers.append(a)
+            number /= a
+        else:
+            a += 1
 
-    while i < len(simple_numbers):
-        if number % simple_numbers[i] == 0:
-            number /= simple_numbers[i]
-            simple_multi.append(simple_numbers[i])
-            continue
-        else: 
-            i += 1
-    
-    print(f"The list of simple multipliers is {simple_multi}")
+    print(f"The multipliers of your number is:", *multipliers)
 
 simple_multipliers()
