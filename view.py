94c5ae0196cf31c9a909afc60c36468eda29a024
Tasha <nataza7394@gmail.com>
2022-11-14 21:46:14 +0300

    Upgraded menu from Artem

--- view.py
+++ view.py
@@ -1,14 +1,35 @@
 # Здесь интерфейс
+from os import system
+
+def start_menu():
+    system('cls')
+    print('Добро пожаловать в калькулятор. С какими числами работаем?')
+    print('1 - Рациональные?')
+    print('2 - Комплексные?')
+    print('3 - Выход')
+    
+    match menu_point:= input('Выберите пункт меню '):
+        case '1':
+            get_value1()
+        case '2':
+            get_value2()
+        case '3':
+            print('Заходите ещё! Всегда будем рады!')
+    if menu_point != '3':
+        start_menu() 
 
 def view(data):
     print(data)
 
-def get_value():
+def get_value1():
     return input("Input you operation. Please separate numbers from the sign of operation with space: ") # число пробел знак пробел число
+
+def get_value2():
+    return input("Input you operation. Please separate numbers from the sign of operation with space: ") # комплексные?
 
 def output(result):
     if str(result).isdigit():
         print(f"Your result is:", *result)
-    elif result % 10 != 0:
-        print(f"Yout result is:", round(result, 2))
     else: print(result)
+
+start_menu()
