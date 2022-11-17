c66c11a32a440265e7783b8ac676ce8b845e6c23
Kris <nataza7394@gmail.com>
2022-11-17 12:31:31 +0300

    I have improved input in view.py

--- view.py
+++ view.py
@@ -1,16 +1,22 @@
 # Здесь интерфейс
 
 def get_value():
-    inp = input("Input you operation. Please separate numbers from the sign of operation with space: ")
-    try_act = inp
-    data = try_act.split()
-    if str(data[0]).isdigit() and str(data[2]).isdigit():
-        if data[1] == "+" or data[1] == "-" or data[1] == "/" or data[1] == "*":
-            return inp
-        else: 
-            print("Yout input is incorrect. Try again, please.")
-            get_value()
-    else: print("...")
+    print("Hello! Welcome to our caculator.")
+    
+    while True:
+        inp = input("Input you operation. Please separate numbers from the sign of operation with space: ")
+        try_act = inp
+        data = try_act.split()
+
+        if len(data) == 3:
+            if str(data[0]).isdigit() and str(data[2]).isdigit():
+                if data[1] == "+" or data[2] == "-" or data[1] == "*" or data[1] == "/":
+                    print("Thank you!")
+                    break
+        else:
+            print("Your input is incorrect. Try again, please.")
+
+    return inp
 
 
 def output(result):
