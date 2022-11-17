e298bc370b14857effbdeccac4676edfa5d4bca2
Kris <nataza7394@gmail.com>
2022-11-17 11:52:51 +0300

    Some changes in menu

--- view.py
+++ view.py
@@ -1,44 +1,19 @@
 # Здесь интерфейс
 
-
-def start_menu():
-    
-    print('Добро пожаловать в калькулятор. С какими числами работаем?')
-    print('1 - Рациональные?')
-    print('2 - Комплексные?')
-    print('3 - Выход')
-    
-    match menu_point:= input('Выберите пункт меню '):
-        case '1':
-            get_value()
-        case '2':
-            get_value()
-        case '3':
-            print('Заходите ещё! Всегда будем рады!')
-    if menu_point != '3':
-        start_menu() 
-
-def view(data):
-    print(data)
-
-
 def get_value():
-    return input("Input you operation. Please separate numbers from the sign of operation with space: ") # число пробел знак пробел число
+    inp = input("Input you operation. Please separate numbers from the sign of operation with space: ")
+    try_act = inp
+    data = try_act.split()
+    if str(data[0]).isdigit() and str(data[2]).isdigit():
+        if data[1] == "+" or data[1] == "-" or data[1] == "/" or data[1] == "*":
+            return inp
+        else: 
+            print("Yout input is incorrect. Try again, please.")
+            get_value()
+    else: print("...")
 
-def srch(inp):
-    result = []
-    n = 0
-    for i in range(len(inp)):
-        if inp[i] in ("+", "-", "*", "/"):
-            result.append(int(inp[n:i]))
-            result.append(inp[i])
-            n = i + 1
-    result.append(int(inp[i:]))
-    return result
 
 def output(result):
     if str(result).isdigit():
         print(f"Your result is:", *result)
     else: print(result)
-
-start_menu()
