2d8e9ad97e02f0f3351960ef8835d9e53016d729
Daria N <107907983+novikovadaria@users.noreply.github.com>
2022-11-17 17:47:49 +0300

    Update log.py

--- log.py
+++ log.py
@@ -5,6 +5,4 @@ operation_time = dt.now().strftime('%H:%M')
 
 
 def logwrite(note, message_text):
-    log_file.write(operation_time + '\n')
-    log_file.write(note + '\n')
-    log_file.write(str(message_text) + '\n')
+    log_file.write(operation_time + ' ' + note + str(message_text) + '\n')
