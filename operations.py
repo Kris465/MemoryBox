0b2c705a35dc265f45975c59b475227912d8f938
Kris <nataza7394@gmail.com>
2022-11-21 02:27:24 +0300

    I managed to avoid complex-method

--- operations.py
+++ operations.py
@@ -3,7 +3,16 @@
 def calc(data):
 
     if 'j' in data:
-        answer = "Complex numbers"
+        print(data)
+        sign = data[4]
+        if sign == "+":
+            answer = str(float(data[0]) + float(data[5])) + "+" + str(float(data[2]) + float(data[7])) + "j"
+        elif sign == "-":
+            answer = str(float(data[0]) - float(data[5])) + "-" + str(float(data[2]) - float(data[7])) + "j"
+        elif sign == "/":
+            answer = str((float(data[0]) * float(data[5]) + float(data[2]) * float(data[7])) / (float(data[5]) ** 2 + float(data[7]) ** 2)) + "+" + str((float(data[5]) * float(data[2]) - float(data[0]) * float(data[7])) / (float(data[5]) ** 2 + float(data[7]) ** 2)) + "j"
+        elif sign == "*":
+            answer = str(float(data[0]) * float(data[5]) - float(data[2]) * float(data[7])) + "+" + str(float(data[0]) * float(data[7]) + float(data[2]) * float(data[5])) + "j"
     else:
         sign = data[1]
     
@@ -15,9 +24,9 @@ def calc(data):
             answer = int(data[0]) * int(data[2])
         elif sign == "/":
             if data[2] == "0":
-                answer = "division for zero"
+                answer = "Деление на ноль!"
             else: answer = int(data[0]) / int(data[2])
-        else: answer = "Something unexpected happened"
+        else: answer = "Нежданчик!"
 
     return answer
 
