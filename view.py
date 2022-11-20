a2403f86f3a506f8bbde798117b251f34db7aa81
Kris <nataza7394@gmail.com>
2022-11-21 00:38:25 +0300

    I have finished with the interface

--- view.py
+++ view.py
@@ -10,27 +10,31 @@ def user_menu():
             [sg.Input()],
             [sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('4'), sg.Button('5'), sg.Button('6'), sg.Button('7'), sg.Button('8'), sg.Button('9'), sg.Button('0')],
             [sg.Button('+'), sg.Button('-'), sg.Button('*'), sg.Button('/'), sg.Button('j')],
-            [sg.Button('='), sg.Button('Exit')]]
+            [sg.Button('Узнать ответ')]]
 
 
     window = sg.Window('Калькулятор', layout)
     action = []
+    flag = True
 
-    while True:
+    while flag == True:
         event, values = window.read()
-        if event == sg.WIN_CLOSED or event == 'Exit':
+        if event == sg.WIN_CLOSED or event == 'Узнать ответ':
             break
         elif event == '0' or '1' or '2' or '3' or '4' or '5' or '6' or '7' or '8' or '9' or '+' or '-' or '/' or '*' or 'j':
             for elem in range(0, 1):
                 action.append(event)
-            sg.Print(*action)
-        elif event == '=':
-            return action
+            sg.Print(event, end = " ")
+        elif event == 'Узнать ответ':
+            flag = False
+    return action
 
-    window.close()
 
 def output(result):
-    sg.Print(result)
+    sg.theme('DarkAmber')
+
+    sg.popup(f'Результат: {result}')
+
     
 
 
