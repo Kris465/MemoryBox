91357ee1518543308b3f537827baf0a21bc84e86
Kris <nataza7394@gmail.com>
2022-11-20 22:48:06 +0300

    I have upgraged interface

--- controller.py
+++ controller.py
@@ -5,8 +5,8 @@ import operations
 import log
 
 def button_click():
-    value_lst, kind_oper = view.get_value()
+    value_lst = view.user_menu()
     log.logwrite("User input: ", value_lst) 
-    result = operations.calc(value_lst, kind_oper)
+    result = operations.calc(value_lst)
     log.logwrite("Result of calculations is: ", result)
     view.output(result)
--- view.py
+++ view.py
@@ -3,26 +3,36 @@
 
 import PySimpleGUI as sg
 
-sg.theme('DarkAmber')
+def user_menu():
+    sg.theme('DarkAmber')
 
-layout = [  [sg.Text('Введите, ваше выражение, пожалуйста: ')],
-            [sg.Print()],
+    layout = [  [sg.Text('Введите, ваше выражение, пожалуйста. \nФормат для целых чисел "2 * 2", формат для комплексных чисел "2 + 3j": ')],
+            [sg.Input()],
             [sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('4'), sg.Button('5'), sg.Button('6'), sg.Button('7'), sg.Button('8'), sg.Button('9'), sg.Button('0')],
             [sg.Button('+'), sg.Button('-'), sg.Button('*'), sg.Button('/'), sg.Button('j')],
-            [sg.Button('Ok'), sg.Button('Exit')]]
+            [sg.Button('='), sg.Button('Exit')]]
 
 
-window = sg.Window('Калькулятор', layout)
+    window = sg.Window('Калькулятор', layout)
+    action = []
 
-while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Exit':
-        break
-    elif event == 
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == 'Exit':
+            break
+        elif event == '0' or '1' or '2' or '3' or '4' or '5' or '6' or '7' or '8' or '9' or '+' or '-' or '/' or '*' or 'j':
+            for elem in range(0, 1):
+                action.append(event)
+            sg.Print(*action)
+        elif event == '=':
+            return action
 
+    window.close()
 
+def output(result):
+    sg.Print(result)
+    
 
-window.close()
 
 # def get_value():
 #     print("Hello! Welcome to our caculator.")
