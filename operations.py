86803947d71628d1789c0bf7ffbedab92ac7c331
Kris <nataza7394@gmail.com>
2022-11-21 00:44:44 +0300

    I have deleted unnecessary parts of script

--- operations.py
+++ operations.py
@@ -1,31 +1,23 @@
 # Создать калькулятор для работы с рациональными и комплексными числами, организовать меню, добавив в неё систему логирования.
 
 def calc(data):
-    sign = data[1]
-    
-    if sign == "+":
-        answer = int(data[0]) + int(data[2])
-    elif sign == "-":
-        answer = int(data[0]) - int(data[2])
-    elif sign == "*":
-        answer = int(data[0]) * int(data[2])
-    elif sign == "/":
-        if data[2] == "0":
-            answer = "division for zero"
-        else: answer = int(data[0]) / int(data[2])
-    else: answer = "Something unexpected happened"
 
-    # else:
-    #     first_num = complex(data[0])
-    #     second_num = complex(data[2])
-    #     if sign == "+":
-    #         answer = first_num + second_num
-    #     elif sign == "-":
-    #         answer = first_num - second_num
-    #     elif sign == "*":
-    #         answer = first_num * second_num
-    #     elif sign == "/":
-    #         answer = first_num / second_num
+    if 'j' in data:
+        answer = "Complex numbers"
+    else:
+        sign = data[1]
+    
+        if sign == "+":
+            answer = int(data[0]) + int(data[2])
+        elif sign == "-":
+            answer = int(data[0]) - int(data[2])
+        elif sign == "*":
+            answer = int(data[0]) * int(data[2])
+        elif sign == "/":
+            if data[2] == "0":
+                answer = "division for zero"
+            else: answer = int(data[0]) / int(data[2])
+        else: answer = "Something unexpected happened"
 
     return answer
 
--- view.py
+++ view.py
@@ -1,5 +1,4 @@
 # Здесь интерфейс
-# Инпут возвращает список с одним действием. Следовательно, интерфейс должен не только получать список действий, но и располагать их в нужном порядке.
 
 import PySimpleGUI as sg
 
@@ -34,34 +33,4 @@ def output(result):
     sg.theme('DarkAmber')
 
     sg.popup(f'Результат: {result}')
-
     
-
-
-# def get_value():
-#     print("Hello! Welcome to our caculator.")
-#     expression = input("Input your expression, please: ")
-#     kind_operation = input("For complex numbers input 1, for usual numbers input 2: ")
-
-#     if kind_operation == 1:
-#         reel_part = input("Input the reel or main part of the first complex number: ")
-#         imaginaire_part = input("Input the imaginaire part of the first complex number: ")
-#         sign = input("Input the sing of operation: ")
-#         reel_part1 = input("Input the reel or main part of the second complex number: ")
-#         imaginaire_part1 = input("Input the imaginaire part of the second complex number: ")
-#         data = [reel_part + imaginaire_part, sign, reel_part1 + imaginaire_part1]
-#     else:
-#         data = expression.split()
-
-#     return data, kind_operation
-
-
-# def output(result):
-#     try:
-#         if result.is_integer():
-#             print(f"Your result is:", int(result))
-#         elif isinstance(result, float):
-#             print(f"Your result is:", result)
-#         else: print(result)
-#     except:
-#         print(result)
