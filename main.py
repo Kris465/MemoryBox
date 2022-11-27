311c8c295997f6a136bb8781a0ef1b8ce98525cb
Kris <nataza7394@gmail.com>
2022-11-27 17:35:35 +0300

    I have imported libs to the project

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+
+
+
+load_dotenv()
+secret_token = os.getenv('TOKEN')
+
+
+
+
+def main():
+    pass
+
+
+
+
+
+
+
+if __name__ == 'main':
+    main()
