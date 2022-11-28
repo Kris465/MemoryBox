89c12ca1f179ceaddfadcfc4e8785789a408afa2
Kris <nataza7394@gmail.com>
2022-11-28 12:53:54 +0300

    some chenges

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
-import os
-from dotenv import load_dotenv
-
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
+import os
+from dotenv import load_dotenv, find_dotenv
+
+
+load_dotenv(find_dotenv())
+token = os.environ.get('TOKEN')
+bot = Bot(token)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def conversation_send(message: types.Message):
+    if message.text == 'Привет':
+        await bot.send_message(message.from_user.id, "И тебе привет!")
+
+    # await message.answer(message.text)
+    # await message.reply(message.text)
+    # await bot.answer_send(message.from_user.id, message.text)
 
 
 
-load_dotenv()
-secret_token = os.getenv('TOKEN')
+executor.start_polling(dp, skip_updates=True)
 
 
-
-
-def main():
-    pass
-
-
-
-
-
-
-
-if __name__ == 'main':
-    main()
+# if __name__ == 'main':
+#     main()
