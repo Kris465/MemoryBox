7f1719a06ba4dd1e0923282a5a890aa0d5f10dc3
Kris <nataza7394@gmail.com>
2022-12-10 15:04:26 +0300

    Chenges

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+from aiogram.types import InputFile
 
 import os
 from dotenv import load_dotenv, find_dotenv
@@ -12,19 +13,25 @@ bot = Bot(token)
 dp = Dispatcher(bot)
 
 
+photo = InputFile("1_prac.png")
+
+
 @dp.message_handler()
 async def conversation_send(message: types.Message):
     if message.text == 'Привет':
         await bot.send_message(message.from_user.id, "И тебе привет!")
-
+    elif message.text == 'Урок':
+        await bot.send_photo(message.from_user.id, photo = photo)
+    elif message.text == 'Проверка':
+        await bot.send_message(message.from_user.id, "Ответы")
+        
+    
     # await message.answer(message.text)
     # await message.reply(message.text)
     # await bot.answer_send(message.from_user.id, message.text)
 
 
-
 executor.start_polling(dp, skip_updates=True)
-
 
 # if __name__ == 'main':
 #     main()
