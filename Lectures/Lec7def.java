c54d5ce63ebc99e7b460bd0ddcf97ab7d2cd3bbb
Kris <nataza7394@gmail.com>
2022-12-12 16:12:38 +0300

    From first lecture

---
+++ Lectures/Lec7def.java
@@ -0,0 +1,13 @@
+package Lectures;
+// Создание функции
+
+public class Lec7def {
+    public static void main(String[] args) {
+        // SayHi();
+        lib.SayHi();
+    }
+
+    // static void SayHi() {
+    //     System.out.println("Hi!");
+    // }
+}
