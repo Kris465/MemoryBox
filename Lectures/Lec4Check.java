dea96c5eaaf265448c0a91d59c15ce3b8a6743b1
Kris <nataza7394@gmail.com>
2022-12-12 15:28:09 +0300

    I have copied some code from lecture

---
+++ Lectures/Lec4Check.java
@@ -0,0 +1,15 @@
+package Lectures;
+import java.util.Scanner;
+// Проверка на соответствие получаемого типа
+
+public class Lec4Check {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("int a: ");
+        boolean flag = iScanner.hasNextInt();
+        System.out.println(flag);
+        int i = iScanner.nextInt();
+        System.out.println(i);
+        iScanner.close();
+    }
+}
