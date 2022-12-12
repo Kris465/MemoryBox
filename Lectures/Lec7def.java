cc9883f8c1755aae6712c58da1abb70af6c0e919
Kris <nataza7394@gmail.com>
2022-12-12 16:42:01 +0300

    If - else, cycles from the first lecture

--- Lectures/Lec7def.java
+++ Lectures/Lec7def.java
@@ -11,3 +11,22 @@ public class Lec7def {
     //     System.out.println("Hi!");
     // }
 }
+
+// Функции и методы:
+
+// public class Program {
+//     static void sayHi() {
+//         System.out.println("hi!");
+//     }
+//     static int sum(int a, int b) {
+//         return a+b;
+//     }  
+//     static double factor(int n) {
+//         if(n==1)return 1;
+//         return n * factor(n-1);
+//     }
+//     public static void main(String[] args) {
+//         sayHi(); // hi!
+//         System.out.println(sum(1, 3)); // 4
+//         System.out.println(factor(5)); // 120.0
+//     }}
---
+++ Lectures/Lec9cycles.java
@@ -0,0 +1,33 @@
+package Lectures;
+// Циклы while
+
+// public class Lec9cycles {
+//     public static void main(String[] args) {
+//         int value = 321;
+//         int count = 0;
+
+//         while (value != 0) {
+//             value /= 10;
+//             count++;
+//         }
+//         System.out.println(count);
+//     }
+
+// }
+
+// do while
+
+// public class Program {
+//     public static void main(String[] args) {
+//         int value = 321;
+//         int count = 0;
+
+//         do {
+//             value /= 10;
+//             count++;
+//         } while (value != 0);
+//         System.out.println(count);
+//     }
+// }
+
+
