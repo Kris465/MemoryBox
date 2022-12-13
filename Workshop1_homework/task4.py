0cc24f3b063df8e7003444ecd9ef29cf12f51ca0
Kris <nataza7394@gmail.com>
2022-12-13 15:20:23 +0300

    The fourth task has been done

--- Workshop1_homework/task4.py
+++ Workshop1_homework/task4.py
@@ -1,1 +1,18 @@
 # Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+def points_in_square():
+    part = int(input("Input the number of the square: "))
+
+    # Код с четвертями, начиная с правой верхней четверти где x > и y > 0, и далее по часовой стрелке (по ответам из прошлого задания)
+
+    if part == 1:
+        print("x is from 0 to infinite, y is from 0 to infinite")
+    elif part == 2:
+        print("x is from -infinite to 0, y is from 0 to infinite")
+    elif part == 3:
+        print("x is from -infinite to 0, y is from -infinite to 0")
+    elif part == 4:
+        print("x is from 0 to infinite, y is from -infinite to 0")
+
+
+points_in_square()
