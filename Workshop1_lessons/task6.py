a9008136f276c322f01c10804fedcce1f25f5802
Kris <nataza7394@gmail.com>
2022-12-13 12:50:20 +0300

    I have created a folder for the first workshop

--- Workshop1_lessons/task6.py
+++ Workshop1_lessons/task6.py
@@ -6,3 +6,5 @@ for x in range(2):
         for z in range(2):
             if not (x == z or x <= y and z):
                 print(x, y, z)
+
+# ДЗ (W ⋀ Z) ⋁ ¬Y ⋁ (¬X ≡ ¬W) \ правый верхний угол, движемся против часовой
