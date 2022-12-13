e1588e66c5e42b1e41224e4091b26ac7d42c14d3
Kris <nataza7394@gmail.com>
2022-12-13 13:12:36 +0300

    The first task has been done

--- Workshop1_homework/task1.py
+++ Workshop1_homework/task1.py
@@ -5,3 +5,14 @@
 # - 6 -> да
 # - 7 -> да
 # - 1 -> нет
+
+def the_weekday():
+    day = int(input("Input the number of the weekday: "))
+
+    if day in range(1, 6):
+        print("No")
+    elif day in range(6, 8):
+        print("Yes")
+    else: print("Error, try again.")
+
+the_weekday()
