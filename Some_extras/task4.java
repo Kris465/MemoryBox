8fff88635e89bf5737639548749b3666e855a9c4
Kris <nataza7394@gmail.com>
2022-12-16 04:28:43 +0300

    some problems

--- Some_extras/task4.java
+++ Some_extras/task4.java
@@ -1,5 +1,16 @@
 package Some_extras;
+import java.util.Scanner;
 
 public class task4 {
-    
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        int first, second, result;
+        System.out.print("Input the first number: ");
+        first = num.nextInt();
+        System.out.print("Input the second number: ");
+        second = num.nextInt();
+        result = first + second;
+        System.out.println("Your result is: " + result);
+        
+    }
 }
--- WorkShop1_Homework/task1.java
+++ WorkShop1_Homework/task1.java
@@ -16,14 +16,16 @@ import java.io.*;
 
 public class task1 {
     public static void main(String[] args) throws Exception {
-        FileReader fr = new FileReader("input.txt");
-        int c;
-        while ((c = fr.read()) != -1) {
-            char ch = (char) c;
-            if (ch == '\n') {
-                System.out.print(ch);
-            } else {
-                System.out.print(ch);
-            }
+        BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+        String str;
+        String[] in_str;
+        Integer numbers;
+        while ((str = br.readLine()) != null) {
+            in_str = str.split("\\D+");
+            numbers = Integer.parseInt(String.join("", in_str));
+            
+            System.out.println(numbers);
         }
-}}
+        br.close();
+    }
+}
