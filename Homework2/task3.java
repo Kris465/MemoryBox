9b736219109c57346235b586474e5df44ec7e13b
Kris <nataza7394@gmail.com>
2022-12-20 23:28:21 +0300

    prime number??? O_o

--- Homework2/task3.java
+++ Homework2/task3.java
@@ -1,5 +1,9 @@
 package Homework2;
 
+// 3) Дана последовательность N целых чисел. Найти сумму простых чисел.
+
 public class task3 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
