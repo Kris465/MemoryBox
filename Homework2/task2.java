fcf9041405b97a5f096a0805d6c2eac295c2b9e4
Kris <nataza7394@gmail.com>
2022-12-20 23:25:06 +0300

    little problem with the second task

--- Homework2/task2.java
+++ Homework2/task2.java
@@ -1,5 +1,26 @@
 package Homework2;
 
+// 2) Дана последовательность целых чисел, оканчивающаяся нулем. Найти сумму положительных чисел, после которых следует отрицательное число.
+
+import java.util.Scanner;
+
 public class task2 {
-    
+    public static void main(String[] args) {
+        int a, b, sum = 0, n;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input the length of number sequence: ");
+        n = scanner.nextInt();
+        System.out.println("Thank you, now input your sequence: ");
+
+        a = scanner.nextInt();
+        for (int i = 1; i < n; i++) {
+            b = scanner.nextInt();
+            if (a > 0 & b < 0) {
+                sum += a;
+            }
+            a = b;
+        }
+        System.out.println("Summa is: " + sum);
+        scanner.close(); // нужен цикл while, по 0 прекращаем ввод, переделать
+    }
 }
