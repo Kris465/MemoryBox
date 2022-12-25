beb1d2703ae2d55c7cadbf3d987f0eb3aad1907e
Kris <nataza7394@gmail.com>
2022-12-26 02:44:43 +0300

    second task has been done

--- Workshop4_homework/task2.py
+++ Workshop4_homework/task2.py
@@ -20,3 +20,18 @@
 # out
 # [2, 5, 5, 13]
 
+def simple_multipliers():
+    number = int(input("Input yout number, please: "))
+    multipliers = []
+    a = 2
+
+    while number > 1:
+        if number % a == 0:
+            multipliers.append(a)
+            number /= a
+        else:
+            a += 1
+
+    print(f"The multipliers of your number is:", *multipliers)
+
+simple_multipliers()
