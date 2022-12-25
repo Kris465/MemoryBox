61ea45c3dd46f68c39a680d13ebe2a555e3fe70d
Kris <nataza7394@gmail.com>
2022-12-26 02:51:04 +0300

    third task has been done

--- Workshop4_homework/task3.py
+++ Workshop4_homework/task3.py
@@ -20,3 +20,29 @@
 # [4, 4, 5, 5, 6, 2, 3, 0, 9, 4]
 # [6, 2, 3, 0, 9]
 
+from random import randint
+
+def creating_a_new_list():
+    size = int(input("Input the size of the list: "))
+    if size <= 0:
+        print("Size can't be less than 0, please, try again!")
+    else:
+        lst = []
+
+        for i in range(size):
+            lst.append(randint(0, 10))
+    
+        print(f"Random list is: {lst}")
+    return lst
+
+def unique_numbers(some_list):
+    lst_unique = []
+
+    for i in some_list: 
+        if i not in lst_unique: 
+            lst_unique.append(i) 
+
+    print(f"Unique numbers are: {lst_unique}")
+            
+random_list = creating_a_new_list()
+unique_numbers(random_list)
