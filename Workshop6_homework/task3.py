c917f2d1969877fb936fdd8f9f0ef7bded2ba621
Kris <nataza7394@gmail.com>
2023-01-18 18:39:59 +0300

    third task has been done

--- Workshop6_homework/task3.py
+++ Workshop6_homework/task3.py
@@ -5,3 +5,11 @@
 # out
 
 # {'А': ['Алина'], 'Б': ['Бибочка'], 'И': ['Иван', 'Илья'], 'М': ['Марина', 'Мария'], 'П': ['Петр', 'Петр']}
+
+def names_dictionary():
+    num = int(input("How many names would you like to input?: "))
+    names = sorted([input() for i in range(num)])
+    dictionary = {name[0]: [name] * names.count(name) for name in names}
+    return dictionary
+
+print(names_dictionary())
