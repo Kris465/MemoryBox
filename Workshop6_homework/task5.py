aea39fe5ae3af303360010516cf12511855e1086
Kris <nataza7394@gmail.com>
2023-01-21 00:26:28 +0300

    fifth task is almost done

--- Workshop6_homework/task5.py
+++ Workshop6_homework/task5.py
@@ -15,37 +15,24 @@
 
 # ['автомобиль ночью мягкий', 'огонь вчера веселый', 'автомобиль позавчера веселый', 'город вчера утопичный', 'лес сегодня зеленый', 'дом вчера яркий', 'автомобиль вчера зеленый', 'огонь позавчера яркий', 'огонь где-то утопичный', 'автомобиль где-то мягкий']
 
-import random
+from random import choice, randrange
 
 def jokes():
     num = int(input("How many jokes would you like to get? "))
-    unique = input("Would you like to get unique jokes? Input 'yes' for agreement: ")
-    if unique == "yes" or "Yes":
-        unique = True
-    else: unique = False
+    unique = bool(input("Would you like to get unique jokes? Input 'True' for agreement: "))
 
     nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
     adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью", "когда-то", "где-то"]
     adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
 
     n, adv, adj = nouns.copy(), adverbs.copy(), adjectives.copy()
- 
-    random.shuffle(n)
-    random.shuffle(adv)
-    random.shuffle(adj)
-    ls = []
+    lst = []
+    list_min = min(n, adv, adj)
 
-    if unique == True and num < len(nouns):
-        ls = list(zip(n, adv, adj))
-        for i in range(0, num):
-            print(ls[i])
-    elif unique == True and num > len(nouns):
-        print("Your number is bigger than I can generate.")
-    else: 
-        for i in range(0, num):
-            k = random.randint(1, 4)
-            ls.append(n[k])
-            ls.append(adv[k + 1])
-            ls.append(adj[k - 1])
+    for i in range(len(list_min) % num if unique else num):
+        number = randrange(len(list_min))
+        lst.append(f"{n.pop(number)} {adv.pop(number)} {adj.pop(number)}" if unique else f"{choice(nouns)} {choice(adverbs)} {choice(adjectives)}")
+
+    print(lst) 
 
 jokes()
