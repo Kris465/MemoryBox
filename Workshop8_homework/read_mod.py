f79748c9b4bc7ac364734429528ad9f47e1c59ce
Kris <nataza7394@gmail.com>
2023-01-26 01:13:15 +0300

    second menu has been finished

--- Workshop8_homework/read_mod.py
+++ Workshop8_homework/read_mod.py
@@ -11,8 +11,12 @@ def reader(argument):
         if argument[0] == "0":
             answer = [row for row in file_reader]
         elif argument[0] == "1":
-            real_arg = int(argument[1:])
-            answer = [row for row in file_reader if real_arg]
+            count = 0
+            for row in file_reader:
+                if count == 0 or count == int(argument[1:]):
+                    answer = (", ".join(row))
+                
+                count += 1
         elif argument[0] == "2":
             real_arg = int(argument[1:])
             answer = [row[(real_arg) - 1] for row in file_reader]
