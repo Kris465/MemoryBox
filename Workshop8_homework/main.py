8fc6189d612ce5c0282efb620df00bd26c13e5e3
Kris <nataza7394@gmail.com>
2023-01-26 00:54:24 +0300

    second menu is almost finished

--- Workshop8_homework/main.py
+++ Workshop8_homework/main.py
@@ -6,28 +6,20 @@ Main part. It controls menus
 import menu
 
 def main():
-    k = 0
-    while k < 100:
-        choic = menu.options()
-        if choic == "1":
-            menu.first_menu(choic)
-        elif choic == "2":
-            wished = menu.second_menu()
-            if wished != False:
-                menu.end_menu(wished)
-            else: continue
-        elif choic == "3":
-            menu.third_menu(choic)
-            break
-        elif choic == "4":
-            menu.fourth_menu(choic)
-            break
-        elif choic == "5":
-            menu.fifth_menu(choic)
-            break
-        elif choic == "6":
-            menu.sixth_menu(choic)
-            break
-        else: continue
+    print("Hello!")
+    choic = menu.options()
+    if choic == "1":
+        menu.first_menu(choic)
+    elif choic == "2":
+        menu.second_menu()
+    elif choic == "3":
+        menu.third_menu(choic)
+    elif choic == "4":
+        menu.fourth_menu(choic)
+    elif choic == "5":
+        menu.fifth_menu(choic)
+    elif choic == "6":
+        menu.sixth_menu(choic)
+    else: k += 1
 
 main()
