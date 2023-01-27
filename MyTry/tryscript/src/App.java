2ed2c8f3d082db6f5b993f4958cfd25f52ec2b9a
Kris <nataza7394@gmail.com>
2023-01-27 08:54:01 +0300

    making constructors

--- MyTry/tryscript/src/App.java
+++ MyTry/tryscript/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
         
-        Cat cat01 = new Cat(); // Создаем экземпляр класса - объект
+        Cat cat01 = new Cat(); // Создаем экземпляр класса - объект, с помощью слова new (тип данных Cat) - (переменная, имя объекта) - (new) - (Конструктор)
     }
 }
