195e69dabf80d3d462aeae912b317256bf7bd3d5
Kris <nataza7394@gmail.com>
2023-02-06 10:36:05 +0300

    one more try

--- Workshop9_homework/main.py
+++ Workshop9_homework/main.py
@@ -10,6 +10,7 @@ from dotenv import load_dotenv, find_dotenv
 import logg
 import mod1
 import mod2
+import check
 
 load_dotenv(find_dotenv())
 token = os.environ.get('TOKEN')
@@ -19,7 +20,7 @@ storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
 class UserState(StatesGroup):
-    operation = State()
+    userstring = State()
 
 @dp.message_handler(commands = ['calc'])
 async def user_register(message: types.Message):
@@ -27,24 +28,37 @@ async def user_register(message: types.Message):
     logg.logging.debug("Program starts")
     await message.answer("Input your statement usually: -2 + 3. \nFor addition: '+' \nFor subtraction: '-' \nFor multiplication: '*' \nFor divining: '/' \nFor divining without  fraction: '//' \nFor fraction from divining: '%' \nFor power: '**' \nFor square root put 's' before the number.\nFor complex numbers you shoul use: -2+3j. Without spaces inside the number. If you don't have any value before j, you should put 1, like: 6+1j.")
     await message.answer("Don't forget to separate '(' and ')' with spaces.\n")
+    await message.answer("Please, input your statement: ")
+    await UserState.operation.set()
 
-@dp.message_handler(state=UserState.operation)
-async def get_useroperation(message: types.Message, state: FSMContext):
+@dp.message_handler(state=UserState.userstring)
+async def get_statement(message: types.Message, state: FSMContext):
+    await state.update_data(userstring = message.text)
     correct_in = "everything is OK?"
+    user_dict = await state.get_data()
+    user_string = user_dict['userstring']
 
-    while isinstance(correct_in, str):
-        await message.answer("Please, input your statement: ")
-        await state.update_data(useroperation = message.text)
-        use_lst = await state.get_data()
-        logg.logging.debug("User's unput has got.")
-        correct_in = check(use_lst)
+    print(user_string, type(user_string))
+
+    logg.logging.debug("User's unput has got.")
+    correct_in = check.checking(str(use_lst))
+
+    print(correct_in)
+    
+    if isinstance(correct_in, str):
+        await message.answer("Your input is incorrect. Try again, please:")
+    else:
+        oper_lst = str(use_lst).split()
+        print(oper_lst)
 
     if correct_in:
-        answer = mod1(use_lst)
+        temp = mod1.cut(oper_lst)
+        answer = mod1.calculator(temp)
     else:
-        answer = mod1(use_lst)
+        temp = mod2.cut(oper_lst)
+        answer = mod2.calculator(temp)
 
-    await message.answer(f'Ваш ответ: {answer}')
+    await message.answer(f'Your answer: {answer}')
 
     await state.finish()
 
