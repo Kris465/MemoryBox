e479b9d947108f0560c74a777724167d8233f997
Kris <nataza7394@gmail.com>
2023-02-06 13:50:52 +0300

    New check - module and main module

--- Workshop9_homework/main.py
+++ Workshop9_homework/main.py
@@ -22,6 +22,7 @@ dp = Dispatcher(bot, storage=storage)
 class UserState(StatesGroup):
     userstring = State()
 
+
 @dp.message_handler(commands = ['calc'])
 async def user_register(message: types.Message):
     await message.answer("Hello! What would you like to count?")
@@ -29,34 +30,31 @@ async def user_register(message: types.Message):
     await message.answer("Input your statement usually: -2 + 3. \nFor addition: '+' \nFor subtraction: '-' \nFor multiplication: '*' \nFor divining: '/' \nFor divining without  fraction: '//' \nFor fraction from divining: '%' \nFor power: '**' \nFor square root put 's' before the number.\nFor complex numbers you shoul use: -2+3j. Without spaces inside the number. If you don't have any value before j, you should put 1, like: 6+1j.")
     await message.answer("Don't forget to separate '(' and ')' with spaces.\n")
     await message.answer("Please, input your statement: ")
-    await UserState.operation.set()
+    await UserState.userstring.set()
+
 
 @dp.message_handler(state=UserState.userstring)
-async def get_statement(message: types.Message, state: FSMContext):
+async def get_userstring(message: types.Message, state: FSMContext):
     await state.update_data(userstring = message.text)
-    correct_in = "everything is OK?"
-    user_dict = await state.get_data()
-    user_string = user_dict['userstring']
+    user_dict = await state.get_data()   
+    current = str(user_dict.get('userstring'))
+    logg.logging.debug(f"User's unput has got. {current}")
+    answer = "nothing"
 
-    print(user_string, type(user_string))
-
-    logg.logging.debug("User's unput has got.")
-    correct_in = check.checking(str(use_lst))
-
-    print(correct_in)
-    
-    if isinstance(correct_in, str):
-        await message.answer("Your input is incorrect. Try again, please:")
+    if current == None:
+        answer = "Try again."
     else:
-        oper_lst = str(use_lst).split()
-        print(oper_lst)
-
-    if correct_in:
-        temp = mod1.cut(oper_lst)
-        answer = mod1.calculator(temp)
-    else:
-        temp = mod2.cut(oper_lst)
-        answer = mod2.calculator(temp)
+        correct_in = check.checking(current)
+        data = current.split()
+        if isinstance(correct_in, str):
+            answer = correct_in
+        elif correct_in:
+            if "s" in data:
+                data = check.pow_remake(data)
+                answer = mod1.calculator(mod1.cut(data))
+            else: answer = mod1.calculator(mod1.cut(data))   
+        else:
+            answer = mod2.calculator(mod2.cut(data))
 
     await message.answer(f'Your answer: {answer}')
 
