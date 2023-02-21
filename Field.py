0b80b8e50e5c0ea19006f23b820e83fbd83640cd
Kris <nataza7394@gmail.com>
2023-02-21 16:39:12 +0300

    some kind of try

--- Field.py
+++ Field.py
@@ -1,2 +1,28 @@
+from random import randint
+from Mob import Mob
+from Player import Player
+
 class Field():
-    pass
+
+    @property
+    def player(self) -> Player:
+        return self.__player
+    
+    @property
+    def mobs(self) -> Mob:
+        return self.__mobs
+    
+    def __init__ (self, size, player, mobs):
+        self.field = ["*" for i in range(size)]
+        self.player = player
+        self.mobs = mobs
+
+
+    def display_info(self):
+        print(f"{self.field}")
+
+John = Player("John", 100, 10)
+Monster = Mob("Monster", 100, 10)
+new_field = Field(25, John, Monster)
+new_field.display_info()
+
