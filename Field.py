cb21d154679192b2ee60d62bf1361bd4db4ed2f6
Kris <nataza7394@gmail.com>
2023-02-21 17:39:10 +0300

    области видимости

--- Field.py
+++ Field.py
@@ -14,14 +14,14 @@ class Field():
     
     def __init__ (self, size, player, mobs):
         self.field = ["*" for i in range(size)]
-        # self.player = player
-        # self.mobs = mobs
+        self.__player = player
+        self.__mobs = mobs
 
     def display_info(self):
         print(f"{self.field}")
 
-# John = Player("John", 100, 10)
-# Monster = Mob("Monster", 100, 10)
-new_field = Field(25)
+John = Player("John", 100, 10)
+Monster = Mob("Monster", 100, 10)
+new_field = Field(25, John, Monster)
 new_field.display_info()
 
--- Player.py
+++ Player.py
@@ -2,6 +2,4 @@ from Character import Character
 
 
 class Player(Character):
-    
-    def display_info(self):
-        print(f"Name: {self.__name} ")
+    pass
