c032c4c4b93224ff3c2195227ee29a9a7ed09de9
Kris <nataza7394@gmail.com>
2023-02-22 03:40:08 +0300

    changed absolutely

--- Field.py
+++ Field.py
@@ -1,3 +1,4 @@
+import os
 from random import randint
 from Mob import Mob
 from Player import Player
@@ -12,11 +13,31 @@ class Field():
     def mobs(self) -> Mob:
         return self.__mobs
     
-    def __init__ (self, player, mob):
-        self.field = [["*" * 5] for i in range(5)]
-        self.field.insert(0, self.__player.name)
-        self.field.insert(randint(0, 25), self.__mob.name)
+    def __init__ (self, player, mobs, size = 10):
+        a = []
+        [a.append(['_'] * size) for _ in range(size)]
+        self.field = a
+        self.__player = player
+        self.__mobs = mobs
+        self.size = size
 
     def display_info(self):
-        [print(i) for i in self.field] 
+        os.system("CLS")
+        print("-" * 50)
+        [print(i) for i in self.field]
 
+
+    def ran(self):
+        return randint(0, self.size-1)
+    
+    def generate(self):
+        return [self.ran(), self.ran()]
+
+    def init_units(self):
+        self.player._place = self.generate()
+        self.field[self.player._place[0]][self.player._place[1]] = self.player._sign
+        for i in self.mobs:
+            i._place = self.generate()
+            self.field[i._place[0]][i._place[1]] = i._sign
+    
+        
