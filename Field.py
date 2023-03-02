070353c2acefc1e367a0b7a316e3197b6e239cf7
lameR <erpresents@gmail.com>
2023-03-03 02:24:32 +0300

    Небольшие изменения

--- Field.py
+++ Field.py
@@ -1,24 +1,23 @@
 import os
 from random import randint
-from Mob import Mob
+
 from Player import Player
 
-class Field():
+
+class Field:
 
     @property
     def player(self) -> Player:
         return self.__player
-    
+
     @property
-    def mobs(self) -> Mob:
+    def mobs(self) -> list:
         return self.__mobs
-    
-    def __init__ (self, player, mobs, size = 10):
-        a = []
-        [a.append(['_'] * size) for _ in range(size)]
-        self.field = a
+
+    def __init__(self, player, mobs, size=10):
+        self.field = [['_'] * size for _ in range(size)]
         self.__player = player
-        self.__mobs = mobs
+        self.__mobs = mobs if isinstance(mobs, list) else [mobs]
         self.size = size
 
     def display_info(self):
@@ -26,18 +25,15 @@ class Field():
         print("-" * 50)
         [print(i) for i in self.field]
 
-
     def ran(self):
-        return randint(0, self.size-1)
-    
+        return randint(0, self.size - 1)
+
     def generate(self):
         return [self.ran(), self.ran()]
 
     def init_units(self):
         self.player._place = self.generate()
-        self.field[self.player._place[0]][self.player._place[1]] = self.player._sign
+        self.field[self.player.place[0]][self.player.place[1]] = self.player.sign
         for i in self.mobs:
             i._place = self.generate()
-            self.field[i._place[0]][i._place[1]] = i._sign
-    
-        
+            self.field[i.place[0]][i.place[1]] = i.sign
