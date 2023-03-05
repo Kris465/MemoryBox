c0696f29ddc513b870a5a58cb04ff76017b180da
Nata <nataza7394@gmail.com>
2023-03-05 20:46:11 +0300

    Player and mob are based classes

--- Player.py
+++ Player.py
@@ -4,10 +4,5 @@ from Character import Character
 class Player(Character):
 
     def __init__(self, name):
-        self._base_hp = 100
-        self._hp = self._base_hp
-        self._name = name
-        self._attack = 10
-        self._sign = "X"
 
         super().__init__(self)
