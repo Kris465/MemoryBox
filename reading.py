d2310c08ea2ffd9709512d5ee3a9bbcb4e5e6833
Nata <nataza7394@gmail.com>
2023-03-09 16:31:18 +0300

    finished

--- reading.py
+++ reading.py
@@ -6,11 +6,11 @@ def read_all():
     data = reader.read()
     
     if menu.id_or_time():
+        return data
+    else:
         lst = data.get("id")
         lst = sorted(lst, key=lambda x: x['time'], reverse=False)
         return lst
-    else:
-        return data
     
 def read_one():
     data = reader.read()
@@ -21,9 +21,15 @@ def read_one():
     else:
         print("Format 09/03/23 1:51")
         ftime = input("What time a you looking for?\n")
-        ftime = datetime.strptime(ftime, "%d/%m/%y %H:%M")
+
+        try:
+            ftime = datetime.strptime(ftime, "%d/%m/%y %H:%M")
+        except:
+            return "Try again. Look at format"
 
         lst = data.get("id")
         for elem in lst:
             if ftime == elem.get("time"):
                 return(elem)
+            else:
+                continue
