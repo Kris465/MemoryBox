f151eecc7bd02cb596dd5af6b90ed2b7ae583c55
Nata <nataza7394@gmail.com>
2023-03-22 00:42:35 +0300

    рекурсия

--- getter_chapter.py
+++ getter_chapter.py
@@ -4,14 +4,12 @@ https://tproger.ru/translations/skraping-sajta-s-pomoshhju-python-gajd-dlja-novi
 
 import requests
 from bs4 import BeautifulSoup
-from re import sub
-from decimal import Decimal
-import io
-from datetime import datetime
-import pandas as pd
 
-URL = 'https://cabinfourtranslations.wordpress.com/2022/07/15/sss-grade-cafe-in-front-of-the-dungeon-chapter-1/'
-html_text = requests.get(URL).text
-soup = BeautifulSoup(html_text, 'lxml')
-ad = soup.find('div', class_ = "entry-content").text
-print(ad)
+def getter_chapter(URL):
+    '''
+    This function gets text from the webpage
+    '''
+    html_text = requests.get(URL).text
+    soup = BeautifulSoup(html_text, 'lxml')
+    ad = soup.find('div', class_ = "entry-content").text
+    return ad
