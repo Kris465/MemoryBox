38daa06c20c56747dba2f69ae94d688b053fe991
Nata <nataza7394@gmail.com>
2023-04-19 14:31:23 +0300

    classes are added

---
+++ translator.py
@@ -0,0 +1,33 @@
+class Translator:
+
+    def __init__(self, name, eng):
+        self.__name = name
+        self.__eng = eng
+        self.__rus = ''
+
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    @property
+    def eng(self):
+        return self.__eng
+    
+    @eng.setter
+    def eng(self, eng):
+        self.__eng = eng
+
+    @property
+    def rus(self):
+        return self.__rus
+    
+    @rus.setter
+    def rus(self, rus):
+        self.__rus = rus
+
+    def info(self):
+        print("I'm a translator")
