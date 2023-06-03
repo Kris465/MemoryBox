ca64a82a9f24906bd85d3f173ebcf6b26877f82f
Nata <nataza7394@gmail.com>
2023-06-03 19:26:43 +0300

    It's working... I think

--- RedBlackTree/src/App.java
+++ RedBlackTree/src/App.java
@@ -1,6 +1,17 @@
 public class App {
-
     public static void main(String[] args) {
-        System.out.println("I'm alive...");
+        RedBlackTree tree = new RedBlackTree();
+        tree.insert(10);
+        tree.insert(20);
+        tree.insert(30);
+        tree.insert(15);
+        tree.insert(18);
+        tree.insert(25);
+        Node found = tree.search(18);
+        if (found != null) {
+            System.out.println("Found node with value " + found.value);
+        } else {
+            System.out.println("Node not found");
+        }
     }
 }
--- RedBlackTree/src/RedBlackTree.java
+++ RedBlackTree/src/RedBlackTree.java
@@ -1,57 +1,132 @@
 public class RedBlackTree {
-    
-    Node root;
-    private class Node{
-        int key;
-        String color;
-        Node left;
-        Node right;
-        Node parent;
+    private Node root;
+
+    public RedBlackTree() {
+        root = null;
     }
 
-    public void add(int key) {
-        Node newNode = new Node(key);
-        if (root == null) {
-            root = newNode;
-            root.color = BLACK;
-            return;
+    private void rotateLeft(Node x) {
+        Node y = x.right;
+        x.right = y.left;
+        if (y.left != null) {
+            y.left.parent = x;
         }
+        y.parent = x.parent;
+        if (x.parent == null) {
+            root = y;
+        } else if (x == x.parent.left) {
+            x.parent.left = y;
+        } else {
+            x.parent.right = y;
+        }
+        y.left = x;
+        x.parent = y;
+    }
+
+    private void rotateRight(Node x) {
+        Node y = x.left;
+        x.left = y.right;
+        if (y.right != null) {
+            y.right.parent = x;
+        }
+        y.parent = x.parent;
+        if (x.parent == null) {
+            root = y;
+        } else if (x == x.parent.right) {
+            x.parent.right = y;
+        } else {
+            x.parent.left = y;
+        }
+        y.right = x;
+        x.parent = y;
+    }
+
+    private void fixViolation(Node node) {
         Node parent = null;
-        Node current = root;
-        while (current != null) {
-            parent = current;
-            if (key < current.key) {
-                current = current.left;
-            } else if (key > current.key) {
-                current = current.right;
+        Node grandparent = null;
+        while (node != root && node.color != Color.BLACK && node.parent.color == Color.RED) {
+            parent = node.parent;
+            grandparent = node.parent.parent;
+            if (parent == grandparent.left) {
+                Node uncle = grandparent.right;
+                if (uncle != null && uncle.color == Color.RED) {
+                    grandparent.color = Color.RED;
+                    parent.color = Color.BLACK;
+                    uncle.color = Color.BLACK;
+                    node = grandparent;
+                } else {
+                    if (node == parent.right) {
+                        rotateLeft(parent);
+                        node = parent;
+                        parent = node.parent;
+                    }
+                    rotateRight(grandparent);
+                    Color temp = parent.color;
+                    parent.color = grandparent.color;
+                    grandparent.color = temp;
+                    node = parent;
+                }
             } else {
-                return; // элемент уже существует
+                Node uncle = grandparent.left;
+                if (uncle != null && uncle.color == Color.RED) {
+                    grandparent.color = Color.RED;
+                    parent.color = Color.BLACK;
+                    uncle.color = Color.BLACK;
+                    node = grandparent;
+                } else {
+                    if (node == parent.left) {
+                        rotateRight(parent);
+                        node = parent;
+                        parent = node.parent;
+                    }
+                    rotateLeft(grandparent);
+                    Color temp = parent.color;
+                    parent.color = grandparent.color;
+                    grandparent.color = temp;
+                    node = parent;
+                }
             }
         }
-        newNode.parent = parent;
-        if (key < parent.key) {
-            parent.left = newNode;
-        } else {
-            parent.right = newNode;
-        }
-        balance(newNode);
+        root.color = Color.BLACK;
     }
 
-    private void rotateLeft(Node node) {
-        Node rightChild = node.right;
-        node.right = rightChild.left;
-        if (rightChild.left != null) {
-            rightChild.left.parent = node;
+    public void insert(int value) {
+        Node node = new Node(value);
+        Node parent = null, current = root;
+
+        while (current != null) {
+            parent = current;
+            if (node.value < current.value) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
         }
-        rightChild.parent = node.parent;
-        if (node.parent == null) {
-            root = rightChild;
-        } else if (node == node.parent.left) {
-            node.parent.left = rightChild;
+
+        node.parent = parent;
+        if (parent == null) {
+            root = node;
+        } else if (node.value < parent.value) {
+            parent.left = node;
         } else {
-            node.parent.right = rightChild;
+            parent.right = node;
         }
-        rightChild.left = node;
-        node.parent = rightChild;
+
+        fixViolation(node);
+    }
+
+    public Node search(int value) {
+        Node current = root;
+        while (current != null && current.value != value) {
+            if (value < current.value) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        return current;
     }
 }
+    
+    
+
