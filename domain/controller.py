c8756d22a7ff933548668ec13b11eb136ed1b0ed
Cherry <nataza7394@gmail.com>
2023-07-20 18:02:59 +0300

    translator modules are added.

--- domain/controller.py
+++ domain/controller.py
@@ -1,4 +1,4 @@
-from database.db_manager import DBManager
+# from database.db_manager import DBManager
 from parser.parser import Parser
 from translator.tr_manager import TRManager
 
@@ -8,22 +8,16 @@ class Controller:
     def logic(self):
         title = input("Title: ")
 
-        option = input("1. Parse\n2. Translate\n")
+        option = int(input("1. Parse\n2. Translate\n"))
         match option:
             case 1:
                 url = input("Url: ")
                 pars = Parser(title=title, project_webpage=url)
                 pars.parse()
             case 2:
-                trans = TRManager()
-                new_option = int(input(
-                    "1. Translate one chapter.\n2. Translate from-to.\n"))
-                match new_option:
-                    case 1:
-                        trans.one_chapter(title)
-                    case 2:
-                        trans.tr_from_to(title)
-
+                language = input("language: ")
+                trans = TRManager(language)
+                trans.tr_from_to(title)
 
         # db_manager = DBManager()
         # project = db_manager.find(title)
