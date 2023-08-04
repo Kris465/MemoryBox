87850ce5ec43a1016e8637bb1901b8dcbbde1272
Kris <nataza7394@gmail.com>
2023-08-04 12:09:34 +0300

    fifth task is done

--- workshop5/task4.py
+++ workshop5/task4.py
@@ -3,6 +3,9 @@
 # числа, сумма цифр которых равна 8.
 # ✔ Решение в одну строку.
 
-even_nums = (num for num in range(0, 101, 2) if sum(int(digit) for digit in str(num)) != 8)
+even_nums = (
+    num for num in range(0, 101, 2)
+    if sum(int(digit) for digit in str(num)) != 8
+    )
 for num in even_nums:
     print(num)
