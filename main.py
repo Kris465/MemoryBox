440d793f3a6bd2d0187dc13400a247213505dde5
Kris <nataza7394@gmail.com>
2023-11-01 22:31:04 +0300

    some changes in logging + some bugs are fixed

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from loguru import logger
 def main():
     logger.add("file.log",
                format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
+               rotation="3 days",
                backtrace=True, diagnose=True)
     controller = Controller()
     controller.logic()
