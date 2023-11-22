07c4533e0e7f71c2cd053a2c2c17b91d7f935a50
Kris <nataza7394@gmail.com>
2023-11-22 17:20:13 +0300

    it is working...

--- domain/task.py
+++ domain/task.py
@@ -1,5 +1,5 @@
 class Task:
-    def __init__(self, title, action, extra, config) -> None:
+    def __init__(self, title, action, extra, config=None) -> None:
         self.title = title
         self.action = action
         self.extra = extra
--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-from domain.controller import Controller
+import asyncio
 from loguru import logger
+
+from domain.controller import Controller
 from user.user_menu import UserMenu
 
 
-def main():
+async def main():
     logger.add("file.log",
                format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
                rotation="3 days",
                backtrace=True, diagnose=True)
 
     user_menu = UserMenu()
-    tasks = user_menu.collect_user_input()
+    tasks = user_menu.menu()
     logger.info(f"Pack of tasks is created: \n"
-                f"{[print(task) for task in tasks]}")
+                f"{[print(task.title) for task in tasks]}")
 
     controller = Controller(tasks)
-    controller.run()
+    await controller.run()
 
 
-if __name__ == 'main':
-    main()
+if __name__ == '__main__':
+    asyncio.run(main())
