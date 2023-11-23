fccf68c8dc0228f1b8907c305812f3eed42fe026
Kris <nataza7394@gmail.com>
2023-11-23 21:12:55 +0300

    zh_shuka is working correctly in async

--- parser/strategies/zh_shuka.py
+++ parser/strategies/zh_shuka.py
@@ -19,18 +19,20 @@ class ChiShuka(ParserStrategy):
         links = await self.collect_links(soup)
         chapters = {}
         for k, v in links.items():
-            text = await self.collect_chapter(v)
+            temp_soup = await self.get_webpage(v)
+            text = await self.collect_chapter(temp_soup)
             chapter = {k: v + text}
             chapters.update(chapter)
         await write(self.title, chapters, language="zh")
 
-    async def collect_chapter(self, url):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
-                page = BeautifulSoup(await response.text(), 'html.parser')
-                chapter = page.find("article", class_="article-content").text
-                logger.info(f"text is collected {chapter[30:]}")
-                return chapter
+    async def collect_chapter(self, soup):
+        try:
+            chapter = soup.find("article", class_="article-content").text
+            logger.info(f"text is collected {chapter[10:]}")
+        except AttributeError:
+            chapter = " "
+            logger.error("Page doesn't have text")
+        return chapter
 
     async def collect_links(self, soup):
         links = {}
