2b951a1cff36020e8598cdca5ecae94b75e65639
Loka <nataza7394@gmail.com>
2024-06-18 21:10:20 +0300

    first script for updating novels

---
+++ cookie.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+url="https://tl.rulate.ru/book/95179"
+login="Leastro"
+password="a_cx8Vbqr7u!6sx"
+
+curl -c cookies.txt $url
---
+++ updator.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+source passwords.cfg
+
+url="https://tl.rulate.ru/book/95179"
+login=$login
+password=$password
+
+response=$(curl -s -X POST -d "login=$login&password=$password" -b "cookies.txt" $url)
+
+# if [[ $response == *"Welcome"* ]]; then
+#     echo "Успешно залогинились!"
+# else
+#     echo "Не удалось залогиниться. Проверьте данные для входа."
+# fi
+
+main_link="https://tl.rulate.ru/book/95179"
+book_number=$(echo "$main_link" | grep -o '[0-9]*$')
+
+all_links=$(echo "$response" | grep -o '<a [^>]*href="[^"]*"' | grep -o 'href="[^"]*"' | cut -d'"' -f2)
+
+matching_links=""
+
+for link in $all_links; do
+    if echo "$link" | grep -q "/book/$book_number"; then
+        matching_links="$matching_links $link"
+    fi
+done
+
+if [ -n "$matching_links" ]; then
+    echo "Ссылки на странице с номером книги $book_number:"
+    echo "$matching_links"
+    echo "$matching_links" > output.txt
+else
+    echo "Ссылки с номером книги $book_number не найдены"
+fi
+
