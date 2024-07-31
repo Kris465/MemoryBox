c83d2d16e761b86b75d0de5ce1b79022e84ae4eb
Krise_laptop <nataza7394@gmail.com>
2024-07-31 13:26:22 +0300

    part of getting webpage is done

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import requests
-from bs4 import BeautifulSoup
 
+url = 'https://tl.rulate.ru/'
+response = requests.get(url)
+if response.ok:
+    with open('page.html', 'w', encoding='utf-8') as file:
+        file.write(response.text)
+    print("Файл сохранен")
+else:
+    print("Не удалось получить код страницы")
