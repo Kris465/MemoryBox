5275d604d03ed7889abc1b51d4be9fd11d6aab1c
Krise_laptop <nataza7394@gmail.com>
2024-07-31 18:33:35 +0300

    requirenments.txt is updated. Loguru library is added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+import loguru
 
 load_dotenv()
 login = os.environ['login']
@@ -12,11 +13,20 @@ response = requests.get(url)
 if response.ok:
     with open('page.html', 'w', encoding='utf-8') as file:
         file.write(response.text)
-    print("Файл сохранен")
 else:
     print("Не удалось получить код страницы")
 
 soup = BeautifulSoup(response.text, 'html.parser')
-login_field = soup.find('div', class_='main-header-avatar').find(
-    'input', name=login)
-print(login_field)
+login_field = soup.find('input', type='text', name='login[login]')
+password_field = soup.find('input', type='password', name='login[pass]')
+
+if login_field and password_field:
+    login_field['value'] = login
+    password_field['value'] = password
+
+    with open('modified_page.html', 'w', encoding='utf-8') as file:
+        file.write(str(soup))
+
+    print("Логин и пароль успешно вставлены в 'modified_page.html'.")
+else:
+    print("Не удалось найти поля для логина или пароля.")
