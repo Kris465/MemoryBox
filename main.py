d3dc8078275ee0b38dea9ebb415c6beceae6f6d5
Krise_laptop <nataza7394@gmail.com>
2024-07-31 19:04:25 +0300

    logging is added, file.log is added to .gitignore

--- main.py
+++ main.py
@@ -2,19 +2,24 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-import loguru
+from loguru import logger
 
 load_dotenv()
 login = os.environ['login']
 password = os.environ['password']
+
+logger.add("file.log",
+           format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
+           rotation='3 days', backtrace=True, diagnose=True)
 
 url = 'https://tl.rulate.ru/'
 response = requests.get(url)
 if response.ok:
     with open('page.html', 'w', encoding='utf-8') as file:
         file.write(response.text)
+    logger("Файл page.html сохранен")
 else:
-    print("Не удалось получить код страницы")
+    logger("Не удалось получить код страницы")
 
 soup = BeautifulSoup(response.text, 'html.parser')
 login_field = soup.find('input', type='text', name='login[login]')
@@ -27,6 +32,6 @@ if login_field and password_field:
     with open('modified_page.html', 'w', encoding='utf-8') as file:
         file.write(str(soup))
 
-    print("Логин и пароль успешно вставлены в 'modified_page.html'.")
+    logger("Логин и пароль успешно вставлены в 'modified_page.html'.")
 else:
-    print("Не удалось найти поля для логина или пароля.")
+    logger("Не удалось найти поля для логина или пароля.")
