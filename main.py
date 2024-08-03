17d0d95ad77b9f07e0f5ed119e59fd4c7a98b5f2
Kris <49696248+Kris465@users.noreply.github.com>
2024-08-03 21:23:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ from dotenv import load_dotenv
 from loguru import logger
 
 load_dotenv()
-login = os.environ['login']
-password = os.environ['password']
+login = os.environ.get('LOGIN')
+password = os.environ.get('PASSWORD')
 
 logger.add("file.log",
            format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
            rotation='3 days', backtrace=True, diagnose=True)
 
-url = 'https://tl.rulate.ru/'
+url = 'https://tl.rulate.ru/register/settings'
 response = requests.get(url)
 if response.ok:
     with open('page.html', 'w', encoding='utf-8') as file:
         file.write(response.text)
-    logger("Файл page.html сохранен")
+    logger.info("Файл page.html сохранен")
 else:
-    logger("Не удалось получить код страницы")
+    logger.error("Не удалось получить код страницы")
 
 soup = BeautifulSoup(response.text, 'html.parser')
 login_field = soup.find('input', type='text', name='login[login]')
@@ -32,6 +32,6 @@ if login_field and password_field:
     with open('modified_page.html', 'w', encoding='utf-8') as file:
         file.write(str(soup))
 
-    logger("Логин и пароль успешно вставлены в 'modified_page.html'.")
+    logger.info("Логин и пароль успешно вставлены в 'modified_page.html'.")
 else:
-    logger("Не удалось найти поля для логина или пароля.")
+    logger.error("Не удалось найти поля для логина или пароля.")
