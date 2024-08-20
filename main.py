4d47937e6dd3c4f24d023e043ea79747cc13aad9
Krise_laptop <nataza7394@gmail.com>
2024-08-20 12:33:54 +0300

    merge was succeed

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ from dotenv import load_dotenv
 from loguru import logger
 
 load_dotenv()
-login = os.environ.get('LOGIN')
-password = os.environ.get('PASSWORD')
+login = os.environ['login']
+password = os.environ['password']
 
 logger.add("file.log",
            format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
            rotation='3 days', backtrace=True, diagnose=True)
 
-url = 'https://tl.rulate.ru/register/settings'
+url = 'https://tl.rulate.ru/'
+# url = 'https://tl.rulate.ru/register/settings'
 response = requests.get(url)
 if response.ok:
     with open('page.html', 'w', encoding='utf-8') as file:
@@ -22,16 +23,17 @@ else:
     logger.error("Не удалось получить код страницы")
 
 soup = BeautifulSoup(response.text, 'html.parser')
-login_field = soup.find('input', type='text', name='login[login]')
-password_field = soup.find('input', type='password', name='login[pass]')
 
-if login_field and password_field:
-    login_field['value'] = login
-    password_field['value'] = password
+login_field = soup.find('input', attrs={'name': 'login[login]'})
+password_field = soup.find('input', attrs={'name: login[pass]'})
 
-    with open('modified_page.html', 'w', encoding='utf-8') as file:
-        file.write(str(soup))
+# if login_field and password_field:
+#     login_field['value'] = login
+#     password_field['value'] = password
 
-    logger.info("Логин и пароль успешно вставлены в 'modified_page.html'.")
-else:
-    logger.error("Не удалось найти поля для логина или пароля.")
+#     with open('modified_page.html', 'w', encoding='utf-8') as file:
+#         file.write(str(soup))
+
+#     logger.info("Логин и пароль успешно вставлены в 'modified_page.html'.")
+# else:
+#     logger.error("Не удалось найти поля для логина или пароля.")
