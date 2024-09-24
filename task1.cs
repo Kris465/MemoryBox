29508f23dc4f1c0b5a87671a349dcdf863048347
Kris <49696248+Kris465@users.noreply.github.com>
2024-09-24 11:30:47 +0300

    Create task1.cs
    C#, first task is finished

---
+++ task1.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+  static void Main() {
+    Console.Write("Введите ваше имя: ");
+    string name = Console.ReadLine();
+    Console.WriteLine($"Привет, {name}!");
+  }
+}
