98807057791f8059eb5b039322fb569e460aba89
Kris <nataza7394@gmail.com>
2024-10-07 00:08:58 +0300

    new files are updated

--- app.py
+++ app.py
@@ -1,26 +1,20 @@
 import os
 import asyncio
-from dotenv import find_dotenv, load_dotenv
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
+
+from dotenv import find_dotenv, load_dotenv
+
+from handlers.user_private import user_private_router
 
 load_dotenv(find_dotenv())
 ALLOWED_UPDATES = ["message", "edited_message"]  # Ограничение апдейтов
 
 bot = Bot(token=os.getenv("TOKEN"))
 dp = Dispatcher()
-dp = Dispatcher()
 
-
-@dp.message(CommandStart())
-async def start_cmd(message: types.Message):
-    await message.answer('Это была команда старт')
-
-
-@dp.message()
-async def echo(message: types.Message):
-    await message.answer(message.text)
-    await message.reply(message.text)
+dp.include_routers(user_private_router)
 
 
 async def main() -> None:
