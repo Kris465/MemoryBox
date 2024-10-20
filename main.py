d582ddd953af1c24b4ea76520bf75b92ddac027f
Kris <nataza7394@gmail.com>
2024-10-21 01:17:11 +0300

    auth is solved

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import os
 from loguru import logger
-from bot import BotUpdator
+import requests
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 
 
 def main():
@@ -7,9 +10,27 @@ def main():
                format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
                rotation='3 days', backtrace=True, diagnose=True)
 
-    bot = BotUpdator()
-    bot.login()
-    bot.fetch_chapters()
+    load_dotenv()
+    login = os.environ['login']
+    password = os.environ['password']
+
+    logger.add("file.log",
+               format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
+               rotation='3 days', backtrace=True, diagnose=True)
+
+    url = 'https://tl.rulate.ru/'
+    url1 = 'https://tl.rulate.ru/register/settings'
+
+    session = requests.Session()
+    session.get(url)
+    print(session.cookies)
+    response = session.post(url, data={'login[login]': login, 'login[pass]': password})
+    print(session.cookies)
+    response1 = session.get(url1)
+    print(session.cookies)
+
+    with open('endfile.html', 'w', encoding='utf-8') as file:
+        file.write(response1.text)
 
 
 if __name__ == '__main__':
