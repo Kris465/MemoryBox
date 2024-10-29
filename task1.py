80369cdf8f8009f46423b54b1f2d8761723bf9c4
unknown <george2748@mail.ru>
2024-10-29 09:49:49 +0300

    isdfjoisdfj

---
+++ task1.py
@@ -0,0 +1,16 @@
+from math import pi
+
+print(round(pi, 2))
+
+from math import e
+
+print(round(e,2))
+
+n = input("введите число:")
+print("вы написали число-n")
+
+print("1 13 49")
+
+print("7  15  100")
+
+print("22   44   66")
