77ea19f496feb42990b767cdf8091bdc3f127ffc
unknown <george2748@mail.ru>
2024-10-29 10:06:15 +0300

    задание 1 выполнено

--- task1.py
+++ task1.py
@@ -1,16 +1,3 @@
 from math import pi
 
 print(round(pi, 2))
-
-from math import e
-
-print(round(e,2))
-
-n = input("введите число:")
-print("вы написали число-n")
-
-print("1 13 49")
-
-print("7  15  100")
-
-print("22   44   66")
