7d318dacc53df2b0afc6a9508cdca504749ffff7
chris <nataza7394@gmail.com>
2024-11-19 15:10:43 +0300

    first line in the firts bot's method is written

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
+from bot import Bot
 from loguru import logger
 import requests
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
 
@@ -14,23 +15,12 @@ def main():
     login = os.environ['login']
     password = os.environ['password']
 
-    logger.add("file.log",
-               format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
-               rotation='3 days', backtrace=True, diagnose=True)
-
     url = 'https://tl.rulate.ru/'
-    url1 = 'https://tl.rulate.ru/register/settings'
 
     session = requests.Session()
     session.get(url)
-    print(session.cookies)
-    response = session.post(url, data={'login[login]': login, 'login[pass]': password})
-    print(session.cookies)
-    response1 = session.get(url1)
-    print(session.cookies)
-
-    with open('endfile.html', 'w', encoding='utf-8') as file:
-        file.write(response1.text)
+    session.post(url, data={'login[login]': login, 'login[pass]': password})
+    bot = Bot(session)
 
 
 if __name__ == '__main__':
