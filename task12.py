e4000e62bfa8d62c9ed44ec8b9cad400928d9d0d
unknown <george2748@mail.ru>
2024-11-26 09:17:53 +0300

    выполнел

---
+++ task12.py
@@ -0,0 +1,3 @@
+kilo = int(intput("введите количество килограмм: "))
+centner = kilo / 100
+print(f'цетренеров: {centner}')
---
+++ task13.py
@@ -0,0 +1,3 @@
+kilo = int(input("введите массу в кило: "))
+ton = kilo / 1000
+print(f'целых тон: {ton}')
