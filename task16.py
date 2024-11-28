91dc87feee584717419f2e703c21d0387e5d222f
AR-15 <george2748@mail.ru>
2024-11-28 11:19:54 +0000

    выполнел

--- task16.py
+++ task16.py
@@ -15,6 +15,7 @@ print(f'a + b/b + c = {(a + b) / (b + c)}')
 print(f'a / sin(b) = {a / sin(b)}')
 print(f'1 / 2 * a * b * sin(x) = {1 / 2 * a * b * sin(x)}')
 print(f'2 * b * c * cos a / sin(x) = {2 * b * c * cos(a / x) / (b + c)}')
-print(f'4 * round * sin(a / 2) * sin(b / 2) * sin(c / 2) = {4 * round * sin(a / 2) * sin(b / 2) * sin(c / 2)}')
-
+print(f'4 * 8.31 * sin(a / 2) * sin(b / 2) * sin(c / 2) = {4 * 8.31 * sin(a / 2) * sin(b / 2) * sin(c / 2)}')
+print(f'a * x * b / c + d = {a * x * b / (c / b)}')
+print(f'2 * sin((a + b) / 2) * cos((a - b) / 2) = {2 * sin((a + b) / 2) * cos((a - b) / 2)}')
 
