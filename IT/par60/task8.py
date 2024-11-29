3be4aa73616a8bacd3a2ae38161f914b7658af60
Kris <oaisjdfoijsf@ijdfoasj>
2024-11-29 15:56:40 +0300

    ...

--- IT/par60/task8.py
+++ IT/par60/task8.py
@@ -4,7 +4,7 @@ n = int(input("Введите натуральное число: "))
 def factorial(n):
     number = 1
     for i in range(1, n+1):
-        number = number * i              
+        number = number * i
     return number
 
 
