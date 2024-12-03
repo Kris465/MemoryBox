1f94f786d889531dfaabcc0811894320449ef180
Kris <oaisjdfoijsf@ijdfoasj>
2024-12-03 13:41:47 +0300

    Сделал

--- python/zadachi/task24.py
+++ python/zadachi/task24.py
@@ -1,5 +1,12 @@
-from math import sqrt, sin
+from math import sin
 
-a = int(input("Введите число a: "))
+# a = int(input("Введите число a: "))
 
-print(f"sqrt(2 * a) + sin(abs(3 * a)) / 3.56 = {sqrt(2 * a) + sin(abs(3 * a)) / 3.56}")
+# print(f"sqrt(2 * a) + sin(abs(3 * a)) / 3.56 = {sqrt(2 * a) + sin(abs(3 * a))
+# / 3.56}")
+
+
+x = int(input("Введите число x: "))
+
+print(f"sin(3.2 + (1 - x)**0.5 / abs(5 * x)) \
+      = {sin((3.2 + (1 - x)**0.5) / abs(5 * x))}")
