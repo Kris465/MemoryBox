ba37bca3881d2245f95232695f2dfd6f045be557
chris <chris@debian.chris>
2024-12-05 11:24:28 +0300

    some changes

---
+++ RPO/IT/par64/task3.py
@@ -0,0 +1,16 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+    return arr
+
+
+array = [64, 34, 25, 12, 22, 11, 90]
+sorted_array = bubble_sort(array)
+print("Отсортированный массив методом пузырька:", sorted_array)
