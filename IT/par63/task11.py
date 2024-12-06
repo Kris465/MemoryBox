3e03ee99f92697369eb21f9ae2a290dddd2aa654
Kris <oaisjdfoijsf@ijdfoasj>
2024-12-06 15:52:13 +0300

    Соломахин

---
+++ IT/par63/task11.py
@@ -0,0 +1,8 @@
+def fibonacci_with_list(n):
+    fib_series = [0, 1]
+    for i in range(2, n):
+        fib_series.append(fib_series[-1] + fib_series[-2])
+    return fib_series
+
+
+print(fibonacci_with_list(20))
