e3f8384866558bc2d6eee17a19baf93ebfe054f6
AR-15 <george2748@mail.ru>
2024-12-10 13:52:57 +0000

    выполнено

---
+++ task31.py
@@ -0,0 +1,10 @@
+num1 = int(input("Введите число: "))
+num2 = int(input("Введите число: "))
+
+print("a)")
+arithmetic_mean = (num1 + num2) / 2
+print(arithmetic_mean)
+
+print("б)")
+geometric_mean = round((num1 * num2) ** 0.5, 2)
+print(geometric_mean)
