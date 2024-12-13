a13cd3a01092da83162ae3cb80c2ab390ee2a077
Kris465 <nataza7394@gmail.com>
2024-12-13 13:05:58 +0300

    ксюша

---
+++ IT/par64/task1.py
@@ -0,0 +1,11 @@
+from random import randint
+
+spisok1 = []
+for i in range(10):
+    spisok1.append(randint(1, 10))
+
+print(*spisok1)
+    
+spisok1.sort()
+print(*spisok1)
+print(len(set(spisok1)))
