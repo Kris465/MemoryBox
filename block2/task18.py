930be0f3113bcbf4eabed70e2a46e6e4bd7c79c9
Kris <49696248+Kris465@users.noreply.github.com>
2024-12-17 12:01:22 +0000

    Кирилл С

--- block2/task18.py
+++ block2/task18.py
@@ -1,8 +1,8 @@
 abc = int(input("Введите число: "))
 n1 = abc // 100
 n2 = abc // 10 % 10
-n3 = abc & 10
-acb= n1 * 100 + n3 * 10 + n2
+n3 = abc % 10
+acb = n1 * 100 + n3 * 10 + n2
 bac = n2 * 100 + n1 * 10 + n3
 bca = n2 * 100 + n3 * 10 + n1
 cab = n3 * 100 + n1 * 10 + n2
@@ -13,4 +13,3 @@ print(bac)
 print(bca)
 print(cab)
 print(cba)
-    
