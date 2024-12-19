8c39df8d9ac5cf39ec6afd90c07758d77e19c13e
Kris <oaisjdfoijsf@ijdfoasj>
2024-12-19 13:59:44 +0300

    ксюша

--- block2/task33.py
+++ block2/task33.py
@@ -1,8 +1,5 @@
-n = int(input("Введите число (1 <= n <= 999): "))
-for last_digit in range(1, 10):
-    two_digit_number = n % 100
-    if two_digit_number // 10 != last_digit:
-        first_digit = two_digit_number % 10
-        x = last_digit * 100 + first_digit * 10 + last_digit
-        if x < 1000:
-            print(x)
+num = input("Введите трёхзначное число: ")
+n1 = num[0]
+n2 = num[1]
+n3 = num[2]
+print(f"{n3}{n2}{n1}")
