0c4003c849453683d5d7fb7cd1b3b14fe1024d37
Kris <nataza7394@gmail.com>
2024-12-19 09:52:28 +0300

    some changes in other files

--- python42/dz14/t1.py
+++ python42/dz14/t1.py
@@ -16,7 +16,8 @@ print("Список 2:", list2)
 combined_list = list1 + list2
 print("Элементы обоих списков:", combined_list)
 
-# 2. Сформировать третий список, содержащий элементы обоих списков без повторений
+# 2. Сформировать третий список,
+# содержащий элементы обоих списков без повторений
 unique_combined_list = list(set(combined_list))
 print("Элементы без повторений:", unique_combined_list)
 
@@ -24,13 +25,15 @@ print("Элементы без повторений:", unique_combined_list)
 common_elements = list(set(list1) & set(list2))
 print("Общие элементы:", common_elements)
 
-# 4. Сформировать третий список, содержащий только уникальные элементы каждого из списков
+# 4. Сформировать третий список,
+# содержащий только уникальные элементы каждого из списков
 unique_list1 = list(set(list1))
 unique_list2 = list(set(list2))
 unique_elements = unique_list1 + unique_list2
 print("Уникальные элементы каждого списка:", unique_elements)
 
-# 5. Сформировать третий список, содержащий только минимальное и максимальное значение каждого из списков
+# 5. Сформировать третий список,
+# содержащий только минимальное и максимальное значение каждого из списков
 min_max_list1 = [min(list1), max(list1)]
 min_max_list2 = [min(list2), max(list2)]
 min_max_combined = min_max_list1 + min_max_list2
