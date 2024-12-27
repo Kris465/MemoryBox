56b1f4a08160e2dfbe47d07924bdee40f56c828a
Kris <oaisjdfoijsf@ijdfoasj>
2024-12-27 10:27:57 +0300

    Маклюсов

--- block3/task19.py
+++ block3/task19.py
@@ -7,17 +7,17 @@ F = False
 G = False
 H = True
 
-print(f"А){not {not A and not B} and A}")
-print(f"{not {not C and not D} and C}")
-print(f"{not {not E and not F} and E}")
-print(f"{not {not G and not H} and G}")
+print(f"А){not (not A and not B) and A}")
+print(f"{not (not C and not D) and C}")
+print(f"{not (not E and not F) and E}")
+print(f"{not (not G and not H) and G}")
 
-print(f"Б){not {not A or not B} and A}")
-print(f"{not {not C or not D} and C}")
-print(f"{not {not E or not F} and E}")
-print(f"{not {not G or not H} and G}")
+print(f"Б){not (not A or not B) and A}")
+print(f"{not (not C or not D) and C}")
+print(f"{not (not E or not F) and E}")
+print(f"{not (not G or not H) and G}")
 
-print(f"В){not {not A or not B} and B}")
-print(f"{not {not C or not D} and D}")
-print(f"{not {not E or not F} and F}")
-print(f"{not {not G or not H} and H}")
+print(f"В){not (not A or not B) and B}")
+print(f"{not (not C or not D) and D}")
+print(f"{not (not E or not F) and F}")
+print(f"{not (not G or not H) and H}")
