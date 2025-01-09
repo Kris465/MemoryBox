adbcee5208850f1010295225d95e3bf2fb13d5b1
Kris <oaisjdfoijsf@ijdfoasj>
2025-01-09 17:14:15 +0300

    some changes

--- Junior/Lesson3/task10.py
+++ Junior/Lesson3/task10.py
@@ -4,7 +4,7 @@ from random import choice
 def random_choice():
     items = input("Введите элементы через запятую: ").split(',')
     chosen_item = choice(items).strip()
-    
+
     print(f"Случайно выбранный элемент: {chosen_item}")
 
 
--- Junior/Lesson3/task9.py
+++ Junior/Lesson3/task9.py
@@ -1,6 +1,6 @@
 def multiplication_table():
     number = int(input("Введите число для вывода таблицы умножения: "))
-    
+
     for i in range(1, 11):
         print(f"{number} x {i} = {number * i}")
 
