861e00168f71b76bb307384f19078d2852e9924e
Kris <oaisjdfoijsf@ijdfoasj>
2025-01-09 16:51:50 +0300

    task8 is done

--- Junior/Lesson3/task8.py
+++ Junior/Lesson3/task8.py
@@ -2,22 +2,23 @@ from random import choice
 
 
 def guess_the_word():
-    words = ["python", "программирование", "машинное", "обучение", "искусственный"]
+    words = ["python", "программирование",
+             "машинное", "обучение", "искусственный"]
     word_to_guess = choice(words)
     guessed_letters = []
-    
+
     while True:
         display_word = ''.join(letter if letter in guessed_letters else '_' for letter in word_to_guess)
         print(f"Слово: {display_word}")
-        
+
         guess = input("Угадайте букву: ").lower()
-        
+
         if guess in word_to_guess:
             guessed_letters.append(guess)
             print("Правильно!")
         else:
             print("Неправильно!")
-        
+
         if all(letter in guessed_letters for letter in word_to_guess):
             print(f"Поздравляем! Вы угадали слово '{word_to_guess}'")
             break
