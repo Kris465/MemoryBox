7a149686092fa2bd418aa778762c7be0e81695fd
Kris <oaisjdfoijsf@ijdfoasj>
2025-01-14 15:19:47 +0300

    fetisov

---
+++ block4/task24.py
@@ -0,0 +1,14 @@
+import math
+
+x = int(input('Введите двухзначное число: '))
+
+a = (x // 10) ** 3
+b = (x % 10) ** 3
+c = x ** 2
+d = (a + b) * 4
+
+if c == d:
+    print('True')
+else:
+    print('False')
+
