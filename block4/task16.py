a8d725dfe69b1f015f66492b4a20e43338b75d41
Kris <oaisjdfoijsf@ijdfoasj>
2025-01-14 13:49:29 +0300

    попов георгий

--- block4/task16.py
+++ block4/task16.py
@@ -1,6 +1,22 @@
-d = 2 * (int(input('введите площадь круга: ')))
-a = (2)*(int(input('введите площадь круга: '))/4)
-if d <= a:
-    print('Входит')
+from math import pi
+d = (int(input('введите площадь круга: ')))
+a = (int(input('введите площадь квадрата: ')))
+diametr = ((d / pi) ** 0.5) * 2
+storona = a ** 0.5
+diogonali = (2 * (storona ** 2)) ** 0.5
+print("а): ")
+
+
+if diametr <= storona:
+    print("круг умещаеться в квадрате")
 else:
-    print('Не входит')
+    print("круг не_умещаеться в квадрате")
+
+
+print("в): ")
+
+
+if diametr >= diogonali:
+    print("квадрат умещаеться в круге")
+else:
+    print("квадрат не_умещаеться в круге")
