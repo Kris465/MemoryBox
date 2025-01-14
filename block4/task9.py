31153610bbe13bfa7b400d736c247efcf1e485b9
Kris <oaisjdfoijsf@ijdfoasj>
2025-01-14 11:55:25 +0300

    Мокряков

--- block4/task9.py
+++ block4/task9.py
@@ -6,3 +6,8 @@ def compare_speeds(km_per_hour, m_per_second):
         print(f"Скорость {m_per_second} м/с больше, чем {km_per_hour} км/ч")
     else:
         print("Скорость равны")
+
+
+mps_from_kph = int(input("Введите скорость км/ч "))
+m_per_second = int(input("Введите скорость м/с "))
+compare_speeds(mps_from_kph, m_per_second)
