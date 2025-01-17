722054f7d9a45a9bed7b07efb3773165251fed79
Kris <oaisjdfoijsf@ijdfoasj>
2025-01-17 15:05:43 +0300

    Maklusov

--- exams/IT/ticket3/main.py
+++ exams/IT/ticket3/main.py
@@ -1,26 +1,25 @@
-number = int(input("Введите трехзначное число: "))
+value = int(input("Введите количество: "))
+from_unit = input("Введите первую единицу измерения (байт, килобайт, \
+    мегабайт, гигабайт)")
+to_unit = input("Введите вторую единицу измерения (байт, килобайт,\
+    мегабайт, гигабайт)")
 
 
-def check_digits(number):
-    str_number = str(number)
+def convert_units(value, from_unit, to_unit):
+    units = {
+        'байт': 1,
+        'килобайт': 1024,
+        'мегабайт': 1024 ** 2,
+        'гигабайт': 1024 ** 3
+    }
 
-    contains_4_7 = '4' in str_number or '7' in str_number
-    contains_3_6_9 = '3' in str_number or '6' in str_number or '9' in \
-        str_number
-
-    return contains_4_7, contains_3_6_9
-
-
-contains_4_7, contains_3_6_9 = check_digits(number)
-
-
-if contains_4_7:
-    print('В числе есть цифры 4 или 7')
-else:
-    print('В цифре отсутствуют цифры 4 или 7')
+    if from_unit in units and to_unit in units:
+        byteval = value * units[from_unit]
+        convval = byteval / units[to_unit]
+        return int(convval)
 
 
-if contains_3_6_9:
-    print('В числе пристутсвтуют цифры 3, 6 или 9')
-else:
-    print('В числе нет цифр 3, 6 или 9')
+result = convert_units(value, from_unit, to_unit)
+
+if result is not None:
+    print(f'{value} {from_unit} = {result} {to_unit} ')
