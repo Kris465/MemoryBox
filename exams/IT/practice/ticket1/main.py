30a7b9d782c79dea9a8dccc64ff034109c44ec40
Kris <oaisjdfoijsf@ijdfoasj>
2025-01-17 09:57:57 +0300

    Манираки не сделано

--- exams/IT/practice/ticket1/main.py
+++ exams/IT/practice/ticket1/main.py
@@ -1,13 +1,32 @@
 text = input("Введите текст: ")
+text1 = text
+text2 = text
 unikslov = 0
 znak = 0
 unicznak = 0
 slov = 0
-cleanline = text
 for i in ",.()!?:;-":
-    resultlne = cleanline.replace(i, "")
-    cleanline = resultlne
+    resultlne = text.replace(i, "")
+    text = resultlne
 print(resultlne)
 spisok = resultlne.split()
-spisok2 = len(spisok)
-print(spisok2)
+dlina = len(spisok)
+print("Количество слов: ", dlina)
+clean_spisok = []
+for i in spisok:
+    if i not in clean_spisok:
+        clean_spisok.append(i)
+print("Уникальных слов: ", len(clean_spisok))
+kolichestvo = 0
+i = 0
+
+while i < len(text1):
+    if text1[i] in ",.()!?:;-":
+        kolichestvo += 1
+    i += 1
+print("Колличество знаков припинания: ", kolichestvo)
+spisok1 = []
+for i in text1:
+    if i not in spisok1 and i in ",.()!?:;-":
+        spisok1.append(i)
+print(spisok1)
