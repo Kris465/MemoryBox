bbf69e3ff3178ba47b14f369d1d57979623ec380
chris <chris@debian.chris>
2025-01-21 20:31:49 +0300

    some changes in files

--- web41/js5/task1.js
+++ web41/js5/task1.js
@@ -1,6 +1,6 @@
 const readline = require('readline');
 
-// Создаем интерфейс для ввода/вывода
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -10,16 +10,16 @@ function stringFrom(...args) {
     return args.join(' ');
 }
 
-// Функция для сбора строк от пользователя
+
 function collectStrings() {
     const strings = [];
 
     function askForInput() {
         rl.question('Введите строку (или "стоп" для завершения): ', (input) => {
             if (input.toLowerCase() === 'стоп') {
-                // Когда пользователь вводит "стоп", вызываем stringFrom
+                
                 console.log(stringFrom(...strings));
-                rl.close(); // Закрываем интерфейс
+                rl.close();
             } else {
                 // Добавляем введенную строку в массив и спрашиваем снова
                 strings.push(input);
--- web41/js5/task2.js
+++ web41/js5/task2.js
@@ -1,11 +1,11 @@
 const prompt = require('prompt-sync')();
 
-// Функция для нахождения минимального значения
+
 function findMinimum(...args) {
     return Math.min(...args);
 }
 
-// Функция для сбора чисел от пользователя
+
 function collectNumbers() {
     const numbers = [];
 
@@ -13,19 +13,19 @@ function collectNumbers() {
         const input = prompt('Введите число (или "стоп" для завершения): ');
 
         if (input.toLowerCase() === 'стоп') {
-            break; // Выход из цикла, если введено "стоп"
+            break;
         }
 
-        const number = parseFloat(input); // Преобразуем ввод в число
+        const number = parseFloat(input);
 
         if (!isNaN(number)) {
-            numbers.push(number); // Добавляем число в массив, если это действительно число
+            numbers.push(number);
         } else {
             console.log('Пожалуйста, введите корректное число или "стоп".');
         }
     }
 
-    // Возвращаем минимальное значение среди введенных чисел
+    
     if (numbers.length > 0) {
         console.log(`Минимальное значение: ${findMinimum(...numbers)}`);
     } else {
@@ -33,5 +33,5 @@ function collectNumbers() {
     }
 }
 
-// Запускаем сбор чисел
+
 collectNumbers();
