9f04c531996e5ce9f80ba1c2e7e49d64bf4f8671
AR-15 <george2748@mail.ru>
2025-01-24 09:10:35 +0000

    выполнено

---
+++ block4/task113.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timedelta
+
+
+def previous_day(m, n):
+    return (datetime(2000, m, n) - timedelta(days=1)).strftime("%d.%m.")
+
+
+def next_day(m, n):
+    return (datetime(2000, m, n) + timedelta(days=1)).strftime("%d.%m.")
+
+
+m = int(input("Введите номер месяца: "))
+n = int(input("Введите число: "))
+
+
+print(f"Предыдущий день: {previous_day(m, n)}")
+print(f"Следующий день: {next_day(m, n)}")
