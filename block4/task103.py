adb51c9f8deeadc327b8efcd2189cf36783c41cb
Kris <49696248+Kris465@users.noreply.github.com>
2025-01-24 06:26:08 +0000

    Кто-то

---
+++ block4/task103.py
@@ -0,0 +1,7 @@
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+num3 = float(input("Введите третье число: "))
+min_ch = min(num1, num2, num3)
+max_ch = max(num1, num2, num3)
+print("Минимальное значение: ", min_ch)
+print("Максимальное значение: ", max_ch)
