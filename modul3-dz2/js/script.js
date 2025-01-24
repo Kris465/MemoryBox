3871fe605ea385bc68ffbea0915a119b3857dc59
Asya <cherriess.info@gmail.com>
2025-01-24 14:06:02 +0300

    dz22

--- modul3-dz2/js/script.js
+++ modul3-dz2/js/script.js
@@ -3,7 +3,6 @@ function openModal() {
     document.getElementById('modalwin').style.display = 'flex';
 }
 
-
 function closeModal() {
     document.getElementById('modalwin').style.display = 'none';
 }
