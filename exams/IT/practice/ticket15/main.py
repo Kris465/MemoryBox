6174aff7edf1a832a86257cd649cf5e4dcdecd86
Kris <oaisjdfoijsf@ijdfoasj>
2025-01-28 14:06:48 +0300

    Манираки не доделал

---
+++ exams/IT/practice/ticket15/main.py
@@ -0,0 +1,18 @@
+text = input("Введите сообщение: ")
+num1 = int(input("Введите свой номер компьютера"))
+num2 = int(input("Ввкедите номер компьютера, куда хотите отправить"))
+
+
+def glav():
+    a = print("Пый")
+    return a
+
+
+glav()
+
+
+def two(a):
+    return 4
+
+
+two()
