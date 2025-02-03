0b761eea0fec3c6152ecd6182be73d2d00d2adc9
chris <chris@debian.chris>
2025-02-03 14:11:25 +0300

    interface is finished

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ from loguru import logger
 
 
 def main():
+    logger.add("file.log",
+               format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
+               rotation="5 days",
+               backtrace=True, diagnose=True)
+
     try:
-        user_data = create_interface()
+        task = create_interface()
     except Exception as e:
         logger.error(e)
+        task = None
 
-    print("Польовательские данные:", user_data)
+    print("Польовательские данные:", task)
 
 
 if __name__ == "__main__":
