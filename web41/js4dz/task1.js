3f3ed2e7c14a3a6b78d7e26ed30bc872c3ff23ba
Kris <nataza7394@gmail.com>
2025-02-04 00:03:33 +0300

    mka lesson5 is done

--- web41/js4dz/task1.js
+++ web41/js4dz/task1.js
@@ -10,10 +10,8 @@ function compareNumbers(num1, num2) {
     }
 }
 
-// Получаем числа от пользователя
 const number1 = parseFloat(prompt('Введите первое число: '));
 const number2 = parseFloat(prompt('Введите второе число: '));
 
-// Сравниваем числа и выводим результат
 const result = compareNumbers(number1, number2);
 console.log(`Результат сравнения: ${result}`);
