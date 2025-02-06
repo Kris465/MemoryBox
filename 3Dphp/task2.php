23a520b726ac38c561999eca4ee71bcf8dccf725
chris <chris@debian.chris>
2025-02-06 21:13:24 +0300

    task are improved, maybe

--- 3Dphp/task2.php
+++ 3Dphp/task2.php
@@ -1,31 +1,17 @@
 <?php
-
 function isPalindrome($string) {
-    // Убираем пробелы и переводим строку в нижний регистр
-    $cleanedString = preg_replace('/s+/', '', mb_strtolower($string));
-    
-    // Убираем все символы, кроме букв (русских и английских)
-    $cleanedString = preg_replace('/[^p{L}]/u', '', $cleanedString);
-    
-    // Проверяем, является ли строка палиндромом
-    return $cleanedString === strrev($cleanedString);
+    $string = mb_strtolower(trim($string));
+    $string = preg_replace('/[^p{L}]/u', '', $string);
+    return $string === strrev($string);
 }
 
-// Основной цикл для пользовательского ввода
-while (true) {
-    echo "Введите строку для проверки на палиндром (или 'exit' для выхода): ";
-    $input = trim(fgets(STDIN)); // Читаем ввод пользователя
+echo "Введите строку: ";
+$handle = fopen("php://stdin", "r");
+$userInput = fgets($handle);
 
-    if (strtolower($input) === 'exit') {
-        break; // Выход из цикла, если пользователь ввел 'exit'
-    }
-
-    if (isPalindrome($input)) {
-        echo "'$input' является палиндромом.\n";
-    } else {
-        echo "'$input' не является палиндромом.\n";
-    }
+if (isPalindrome($userInput)) {
+    echo "Строка является палиндромом.\n";
+} else {
+    echo "Строка не является палиндромом.\n";
 }
-
-echo "Выход из программы.\n";
 ?>
