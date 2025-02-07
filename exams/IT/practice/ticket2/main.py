f6faa2f74fe09144e74d4984c4e1ee8aed16294b
chris <chris@debian.chris>
2025-02-07 10:17:58 +0300

    ticket two is done

--- exams/IT/practice/ticket2/main.py
+++ exams/IT/practice/ticket2/main.py
@@ -1,36 +1,48 @@
 from random import randint
 
-
 def triple_characters(s):
+    """Утроить каждый символ в строке."""
     return ''.join([char * 3 for char in s])
 
+def simulate_transmission(tripled_string):
+    """Имитация передачи сообщения с случайными удалениями символов."""
+    transmitted_message = []
+    i = 0
+    
+    while i < len(tripled_string):
+        segment = tripled_string[i:i + 4]
+        
+        if len(segment) == 4:
+            k = randint(0, 2)
+            if k == 0:
+                transmitted_message.append(segment[:-1])
+            elif k == 1:
+                transmitted_message.append(segment[1:])
+            else:
+                transmitted_message.append(segment[0:2] + segment[3:])
+        else:
+            transmitted_message.append(segment)
 
-original_string = input('Введите ваше сообощение: ')
-tripled_string = triple_characters(original_string)
-a = []
-for f in range(10):
-    flag = randint(1, len(original_string))
-i = 0
-k = randint(0, 2)
+        i += 4
+
+    return ''.join(transmitted_message)
+
+def remove_duplicates(s):
+    """Удалить дублирующиеся символы из строки."""
+    return ''.join([s[i] for i in range(len(s) - 1) if s[i] != s[i + 1]]) + s[-1]
+
+def main():
+    original_string = input('Введите ваше сообщение: ')
+    tripled_string = triple_characters(original_string)
+    
+    print(f"Утроенное сообщение: {tripled_string}")
+    
+    redflag = simulate_transmission(tripled_string)
+    print(f"Полученное сообщение с помехами: {redflag}")
+    
+    greenflag = remove_duplicates(redflag)
+    print(f"Сообщение без дубликатов: {greenflag}")
 
 
-while i < len(tripled_string):
-    b = tripled_string[i:i+4]
-    if k == 0:
-        a.append(b[:-1])
-    elif k == 1:
-        a.append(b[1:])
-    else:
-        e = b[0:2]
-        w = b[3:5]
-        a.append(e + w)
-    i += 4
-
-redflag = ''.join(a) + ' '
-greenflag = []
-for i in range(0, len(redflag) - 1):
-    if redflag[i] != redflag[i+1]:
-        greenflag.append(redflag[i])
-
-print(redflag)
-print(''.join(greenflag))
+if __name__ == "__main__":
+    main()
