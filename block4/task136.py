19a9c2f95cc0d25beed63f027fd8ca5874907cc1
Kris <49696248+Kris465@users.noreply.github.com>
2025-02-07 08:55:26 +0000

    Манираки исправил

--- block4/task136.py
+++ block4/task136.py
@@ -1,12 +1,9 @@
 def get_day_of_week(k):
-    days = ["Ponedelnik", "Vtornik", "Sreda", "Chetverg", "Pyatnitsa", "Subbota", "Voskresenie"]
-
-
+    days = ["Ponedelnik", "Vtornik", "Sreda", "Chetverg",
+            "Pyatnitsa", "Subbota", "Voskresenie"]
     if not (1 <= k <= 365):
         print("Число должно быть от 1 до 365")
         return
-    
-
     day_index = k % 7
     print(days[day_index])
 
