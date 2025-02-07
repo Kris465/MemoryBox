f02e11267a78cd91ae5ab7798d84ebbaffa79eec
chris <chris@debian.chris>
2025-02-07 11:53:47 +0300

    ticket 12 is done

--- exams/IT/practice/ticket12/main.py
+++ exams/IT/practice/ticket12/main.py
@@ -1,18 +1,42 @@
-def main():
-    user_input=input("Введите что-то: ")
-   
+def determine_type(value):
+    if value.lower() in ['true', 'false']:
+        return f"Тип: {type(value.lower() == 'true').__name__} (логическое значение)"
+
     try:
-       float_value = float(user_input)
-       if '.' in user_input or 'e' in user_input.lower():
-           print(f"Тип: {type(float_value).__name__}(число с плавающей запятой)")
-       else:
-           print(f"Тип:{type(int(float_value)).__name__}(целое число)")
+        int_value = int(value)
+        return f"Тип: {type(int_value).__name__} (целое число)"
     except ValueError:
-        if user_input.lower() in ['true', 'false']:
-            print(f"Тип:{type(user_input.lower() == 'true').__name__}(логическое значение)")
-        else:
-            print(f"Тип: {type(user_input).__name__}")
-            
+        pass
+
+    try:
+        float_value = float(value)
+        if '.' in value or 'e' in value.lower():
+            return f"Тип: {type(float_value).__name__} (число с плавающей запятой)"
+    except ValueError:
+        pass
+
+    if value.startswith("[") and value.endswith("]"):
+        try:
+            list_value = eval(value)
+            if isinstance(list_value, list):
+                return f"Тип: {type(list_value).__name__} (список)"
+        except Exception:
+            pass
+
+    if value.startswith("(") and value.endswith(")"):
+        try:
+            tuple_value = eval(value)
+            if isinstance(tuple_value, tuple):
+                return f"Тип: {type(tuple_value).__name__} (кортеж)"
+        except Exception:
+            pass
+    return f"Тип: {type(value).__name__} (строка)"
+
+def main():
+    user_input = input("Введите что-то: ")
+    result = determine_type(user_input)
+    print(result)
 
 
-main()
+if __name__ == "__main__":
+    main()
