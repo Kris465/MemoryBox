912004074cc81e63b4e519a0841f21a6a97612c1
chris <chris@debian.chris>
2025-02-07 11:38:09 +0300

    ticket 10 is done

--- exams/IT/practice/ticket10/main.py
+++ exams/IT/practice/ticket10/main.py
@@ -1,25 +1,33 @@
+def to_boolean(value):
+    if value.lower() == 'true':
+        return True
+    elif value.lower() == 'false':
+        return False
+    else:
+        raise ValueError("Некорректное значение. Пожалуйста, введите True или False.")
+
+
 def main():
-    first_operand = input("Введите первое логичское значение (True/False): ")
-    second_operand = input("Введите второе логическое значение (True/False): ")
-    
-    if first_operand.lower() == 'true':
-        first_operand = True
-    else:
-        first_operand = False
-    if second_operand.lower() == 'true':
-        second_operand = True
-    else:
-        second_operand = False
+    try:
+        first_operand = input("Введите первое логическое значение (True/False): ")
+        second_operand = input("Введите второе логическое значение (True/False): ")
 
-    and_result = first_operand and second_operand
-    or_result = first_operand or second_operand
-    not_first_result = not first_operand
-    not_second_result = not second_operand
+        first_operand = to_boolean(first_operand)
+        second_operand = to_boolean(second_operand)
 
-    print(f"Результат AND: {and_result}")
-    print(f"Результат OR: {or_result}")
-    print(f"Результат NOT первого операнда: {not_first_result}")
-    print(f"Результат NOT второго опернда: {not_second_result}")
+        and_result = first_operand and second_operand
+        or_result = first_operand or second_operand
+        not_first_result = not first_operand
+        not_second_result = not second_operand
+
+        print(f"Результат AND: {and_result}")
+        print(f"Результат OR: {or_result}")
+        print(f"Результат NOT первого операнда: {not_first_result}")
+        print(f"Результат NOT второго операнда: {not_second_result}")
+
+    except ValueError as e:
+        print(e)
 
 
-main()
+if __name__ == "__main__":
+    main()
