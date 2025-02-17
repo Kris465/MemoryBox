2a6ee59b6a7519145f23155e42c5f519bf461a17
Kris465 <nataza7394@gmail.com>
2025-02-17 11:30:53 +0300

    Маклюсов

--- block5/task51.py
+++ block5/task51.py
@@ -1,5 +1,5 @@
 def mixed_average(numbers, n):
-    return (sun(numbers) + n) / (len(numbers) + 1)
+    return (sum(numbers) + n) / (len(numbers) + 1)
 
 
 print("среднее число 1, 2, 3, 4 и числа 4 равно:",
