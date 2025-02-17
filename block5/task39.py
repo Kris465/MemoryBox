089ee28e605c2c4cbc601b239c5f307a43b1100d
Kris465 <nataza7394@gmail.com>
2025-02-17 09:47:27 +0300

    Соломахин

---
+++ block5/task39.py
@@ -0,0 +1,20 @@
+a1 = int(input("Введите число: "))
+a2 = int(input("Введите число: "))
+a3 = int(input("Введите число: "))
+a4 = int(input("Введите число: "))
+a5 = int(input("Введите число: "))
+a6 = int(input("Введите число: "))
+a7 = int(input("Введите число: "))
+a8 = int(input("Введите число: "))
+a9 = int(input("Введите число: "))
+a10 = int(input("Введите число: "))
+sum = 0
+for i in a1, a2, a3, a4, a5, a6, a7, a8, a9, a10:
+    sum += i 
+print(sum)
+
+sum = 0
+for i in range(10):
+    number = int(input())
+    sum += number
+print(sum)
