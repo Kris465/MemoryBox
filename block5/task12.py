9689850912b4fecde909e203e2491f334f956a8e
AR-15 <george2748@mail.ru>
2025-02-28 23:33:06 +0300

    Update task12.py

--- block5/task12.py
+++ block5/task12.py
@@ -13,4 +13,4 @@ print("| Высота (м) | Плотность (кг/м³) |")
 print("|------------|--------------------|")
 for h in range(0, 1100, 100):
     density = calculate_density(h)
-    print(f"| {h:<10} | {density:.2f}            |")
+    print(f"| {h:<10} | {density:.2f} |")
