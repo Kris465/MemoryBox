27140a8feafb4a0aa153689c4ad6d945694db348
Kris465 <nataza7394@gmail.com>
2025-03-10 09:06:34 +0300

    Сделал

--- block6/task97.py
+++ block6/task97.py
@@ -4,16 +4,15 @@ def find_place(sorted_points, target_points):
     while left <= right:
         mid = (left + right) // 2
         if sorted_points[mid] == target_points:
-            return mid + 1  # +1, так как индексация начинается с 0
+            return mid + 1
         elif sorted_points[mid] < target_points:
             right = mid - 1
         else:
             left = mid + 1
 
-    return -1  # Если значение не найдено
+    return -1
 
 
-# Пример использования
 sorted_points = [56, 54, 53, 50, 49, 48, 47, 45, 44, 43, 42, 40, 39, 38, 37,
                  36, 35, 34, 33, 32]
 target_points = 47
