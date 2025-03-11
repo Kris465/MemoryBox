5b823f127b767bc3545e29375bc96905683ee6dc
Kris465 <nataza7394@gmail.com>
2025-03-11 14:13:47 +0300

    вадим

--- block7/task19.py
+++ block7/task19.py
@@ -1,4 +1,5 @@
-numbers = [float(x) for x in input("Введите 15 вещественных чисел через пробел: ").split()]
+numbers = [float(x) for x in input(
+    "Введите 15 вещественных чисел через пробел: ").split()]
 
 total_sum = 0
 
