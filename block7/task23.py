14167592c754cf3a345619e31d818ccd8ef7115c
Kris465 <nataza7394@gmail.com>
2025-03-13 13:41:08 +0300

    солонченко кирилл

--- block7/task23.py
+++ block7/task23.py
@@ -1,8 +1,7 @@
-# Данные о количестве осадков за каждый день месяца
 precipitation = [5, 10, 0, 8, 12, 7, 0, 9, 11, 6, 0, 10, 8, 12, 7, 0, 9, 11,
                  6, 0, 10, 8, 12, 7, 0, 9, 11, 6, 0, 10]
 
-# Подсчет осадков за четные дни (второй, четвертый и т.д.)
-total_precipitation = sum(precipitation[i] for i in range(1, len(precipitation
-                                                                 ), 2))
+total_precipitation = sum(filter(lambda x: precipitation.index(x) % 2 == 0,
+                                 precipitation))
+
 print("Общее количество осадков за четные дни месяца:", total_precipitation)
