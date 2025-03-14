764a076c00f0356e0257b307a9b95eea73a0cbf4
Kris465 <nataza7394@gmail.com>
2025-03-14 09:11:42 +0300

    ксюша

---
+++ block7/task23.py
@@ -0,0 +1,12 @@
+precipitation = [0.5, 0.0, 1.2, 0.8, 0.0, 0.6, 2.3, 0.0, 1.0, 0.4, 
+                 0.2, 0.0, 0.3, 0.5, 0.0, 0.1, 0.4, 0.0, 0.6, 0.0, 
+                 1.5, 0.0, 0.2, 0.3, 0.0, 0.7, 0.1, 0.0, 0.4, 0.5]
+
+total_precipitation = 0.0
+
+
+for i in range(1, len(precipitation), 2):
+    total_precipitation += precipitation[i]
+
+print(f"Общее количество осадков, выпавших 2-го, 4-го и т.д. числа месяца: \
+    {total_precipitation} мм")
