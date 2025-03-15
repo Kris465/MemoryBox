7399d3a8a2bcba39184f15487529373089701508
Kris <nataza7394@gmail.com>
2025-03-15 20:22:09 +0300

    some changes

--- web41/js3/task4.js
+++ web41/js3/task4.js
@@ -2,7 +2,7 @@ const prompt = require('prompt-sync')();
 
 let rate = parseFloat(prompt('Введите годовую депозитную ставку (в процентах): '));
 let years = 0;
-let amount = 1; // Начальная сумма
+let amount = 1;
 
 while (amount < 2) {
     amount *= (1 + rate / 100);
