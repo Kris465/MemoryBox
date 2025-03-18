0681085f7f0b8f3abe17b14f0d9e33ba022c0371
Kris465 <nataza7394@gmail.com>
2025-03-18 13:37:52 +0300

    Маклюсов

--- block7/task112.py
+++ block7/task112.py
@@ -7,8 +7,10 @@ if len(boys_heights) > 0 and len(girls_heights) > 0:
     avg_boy_height = abs(sum(boys_heights)) / len(boys_heights)
     avg_girl_height = sum(girls_heights) / len(girls_heights)
     if avg_boy_height > avg_girl_height + 10:
-        print("Средний рост мальчиков превышает средний рост девочек более чем на 10 см.")
+        print("Средний рост мальчиков превышает средний рост девочек\
+            более чем на 10 см.")
     else:
-        print("Средний рост мальчиков не превышает средний рост девочек более чем на 10 см.")
+        print("Средний рост мальчиков не превышает средний рост девочек\
+              более чем на 10 см.")
 else:
     print("Недостаточно данных для расчета.")
