592375cda832d77e260f79cc48d0b8e3ffb4d64a
Kris465 <nataza7394@gmail.com>
2025-03-21 13:15:53 +0300

    Маклюсов

---
+++ block8/task32.py
@@ -0,0 +1,14 @@
+def sieve_of_eratosthenes(limit):
+    sieve = [True] * (limit + 1)
+    sieve[0] = sieve[1] = False
+    for i in range(2, int(limit ** 0.5) + 1):
+        if sieve[i]:
+            for j in range(i*i, limit + 1, i):
+                sieve[j] = False
+    return [i for i, is_prime in enumerate(sieve) if is_prime]
+
+
+prime_numbers = sieve_of_eratosthenes(541)[:100]
+
+# Выводим результат
+print(prime_numbers)
