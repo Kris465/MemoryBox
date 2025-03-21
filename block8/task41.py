03bde829e3a584f7fefcfb2efbed75b4580005ef
Kris465 <nataza7394@gmail.com>
2025-03-21 14:46:56 +0300

    ксюша

--- block8/task41.py
+++ block8/task41.py
@@ -1,7 +1,7 @@
 def find_natural_solutions():
     solutions = set()
     for x in range(1, 31):
-        for y in range(x, 31):  # чтобы избежать повторений при перестановке x и y
+        for y in range(x, 31):
             k = 2 ** x + 2 ** y
             if 1 <= k <= 30:
                 solutions.add((x, y, k))
