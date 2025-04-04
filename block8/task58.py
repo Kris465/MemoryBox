9aa1a1d77d3227c458fe19e0a47ea317f577151e
chris <nataza7394@gmail.com>
2025-04-04 14:07:08 +0300

    some kind of update

--- block8/task58.py
+++ block8/task58.py
@@ -1,19 +1,21 @@
-def find_smallest_taxicab_number():
-    sums_of_cubes = {}
+def find_smallest_n():
+    sums = {}
+    limit = 100
+    for a in range(1, limit):
+        for b in range(a, limit):
+            sum_of_cubes = a**3 + b**3
+            if sum_of_cubes not in sums:
+                sums[sum_of_cubes] = []
+            sums[sum_of_cubes].append((a, b))
 
-    for a in range(1, 100):
-        for b in range(a, 100):
-            s = a**3 + b**3
-            if s in sums_of_cubes:
-                sums_of_cubes[s].append((a, b))
-            else:
-                sums_of_cubes[s] = [(a, b)]
+    smallest_n = None
+    for sum_of_cubes, pairs in sums.items():
+        if len(pairs) > 1:
+            if smallest_n is None or sum_of_cubes < smallest_n:
+                smallest_n = sum_of_cubes
 
-    for s in sorted(sums_of_cubes.keys()):
-        if len(sums_of_cubes[s]) >= 2:
-            return s, sums_of_cubes[s]
+    return smallest_n
 
 
-result, representations = find_smallest_taxicab_number()
-print(f"Наименьшее число: {result}")
-print("Представления:", representations)
+result = find_smallest_n()
+print("Наименьшее натуральное число n:", result)
