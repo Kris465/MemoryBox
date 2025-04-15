c2c57219d61560c79ed25eabcd527dae95974ff6
Kris465 <nataza7394@gmail.com>
2025-04-15 13:26:42 +0300

    Вадим

---
+++ block9/task29.py
@@ -0,0 +1,9 @@
+word = "клоун"
+
+yklon = word[3] + word[0] + word[1] + word[2] + word[4]
+kylon = word[0] + word[3] + word[1] + word[2] + word[4]
+kolyn = word[0] + word[2] + word[1] + word[3] + word[4]
+
+print(f"уклон: {yklon}")
+print(f"кулон: {kylon}")
+print(f"колун: {kolyn}")
