832934b2326420c14b99afd971361a13ac7b91d0
Kris465 <nataza7394@gmail.com>
2025-04-17 14:05:45 +0300

    popov george

---
+++ block9/task65.py
@@ -0,0 +1,16 @@
+sentence = input()
+
+
+count_ro = sentence.count("ро")
+
+
+pair = input()
+count_pair = sentence.count(pair)
+
+
+substring = input()
+count_substring = sentence.count(substring)
+
+print(count_ro)
+print(count_pair)
+print(count_substring)
