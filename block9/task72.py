2ee0e7085452ef03d62d6e3e55229ac5d7b75f85
Kris465 <nataza7394@gmail.com>
2025-04-17 15:03:14 +0300

    Соломахин

---
+++ block9/task72.py
@@ -0,0 +1,5 @@
+sentence = "мама мыла раму"
+world_count = len(sentence.split())
+is_more_than_three = world_count > 3
+print(f"Число слов: {world_count}")
+print(f"Больше трех? {is_more_than_three}")
