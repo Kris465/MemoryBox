28e79c08ce3de0c4c7684901032d2f0954c42204
Kris465 <nataza7394@gmail.com>
2025-04-18 13:38:23 +0300

    george

--- block9/task107.py
+++ block9/task107.py
@@ -5,7 +5,9 @@ last_o_index = slovo.rfind('о')
 
 if first_a_index != -1 and last_o_index != -1:
     word_list = list(slovo)
-    word_list[first_a_index], word_list[last_o_index] = word_list[last_o_index], word_list[first_a_index]
+    word_list[first_a_index],
+    word_list[last_o_index] = word_list[last_o_index],
+    word_list[first_a_index]
     modified_word = ''.join(word_list)
 else:
     modified_word = slovo
