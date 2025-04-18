c9435a496c58499808ad7e9e115aac70b4ab64dc
Kris465 <nataza7394@gmail.com>
2025-04-18 13:10:33 +0300

    ksusha

---
+++ block9/task99.py
@@ -0,0 +1,8 @@
+sentence = input("Введите предложение: ")
+
+s1 = input("Введите подстроку, которую хотите заменить: ")
+s2 = input("Введите подстроку, на которую будете менять: ")
+
+result_sentence = sentence.replace(s1, s2)
+
+print(result_sentence)
