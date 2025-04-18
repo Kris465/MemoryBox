816c317e07b8b571a518ff9c60a8c2ae0be4d461
Kirill <kirillvdovin911@gmail.com>
2025-04-18 15:06:37 +0300

    commit

--- student_management/student_management/settings.py
+++ student_management/student_management/settings.py
@@ -11,10 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
