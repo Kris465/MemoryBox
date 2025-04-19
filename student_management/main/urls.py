0bab2473df08b73740ecf54f85e6771236c5c2b4
Kirill <kirillvdovin911@gmail.com>
2025-04-19 11:07:23 +0300

    some changes

--- student_management/main/urls.py
+++ student_management/main/urls.py
@@ -1,8 +1,11 @@
 # main/urls.py
+from django.conf import settings
 from django.urls import path
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
+
 from . import views
 
 
@@ -33,4 +36,5 @@ urlpatterns = [
     path('student/grades/', views.student_grades, name='student_grades'),
     path('favicon.ico', RedirectView.as_view
          (url=staticfiles_storage.url('img/favicon.ico')))
-]
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- student_management/student_management/settings.py
+++ student_management/student_management/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -115,8 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '%static/'
-STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
