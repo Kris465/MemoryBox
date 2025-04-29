67b28e8e2d610c819b7d78975103c2a973394401
Kris465 <nataza7394@gmail.com>
2025-04-29 13:24:31 +0300

    Арсений Степанов

--- block10/task2.py
+++ block10/task2.py
@@ -2,30 +2,30 @@ import math
 
 
 def calculate_y_a():
-    y_a = (((1 + math.sin(1)) / 3) + 
-            ((5 + math.sin(5)) / 3) + 
-            ((3 + math.sin(3)) / 3))
+    y_a = (((1 + math.sin(1)) / 3) +
+           ((5 + math.sin(5)) / 3) +
+           ((3 + math.sin(3)) / 3))
     return y_a
 
 
 def calculate_y_b():
-    y_b = (((2 + math.sin(2)) / (math.sin(5) + 5)) + 
-            ((6 + math.sin(6)) / (math.sin(3) + 3)) + 
-            ((1 + math.sin(1)) / (math.sin(4) + 4)))
+    y_b = (((2 + math.sin(2)) / (math.sin(5) + 5)) +
+           ((6 + math.sin(6)) / (math.sin(3) + 3)) +
+           ((1 + math.sin(1)) / (math.sin(4) + 4)))
     return y_b
 
 
 def calculate_y_c():
-    y_c = (((1 + math.sin(4)) / (4 + math.sin(1))) + 
-            ((7 + math.sin(5)) / (5 + math.sin(7))) + 
-            ((3 + math.sin(2)) / (2 + math.sin(3))))
+    y_c = (((1 + math.sin(4)) / (4 + math.sin(1))) +
+           ((7 + math.sin(5)) / (5 + math.sin(7))) +
+           ((3 + math.sin(2)) / (2 + math.sin(3))))
     return y_c
 
 
 def calculate_y_d():
     y_d = (((2 + math.sin(3)) / (3 + math.sin(2))) +
-            ((1 + math.sin(5)) / (math.sin(1) + 5)) +
-            ((math.sin(7) + 4) / (math.sin(3) + 7)))
+           ((1 + math.sin(5)) / (math.sin(1) + 5)) +
+           ((math.sin(7) + 4) / (math.sin(3) + 7)))
     return y_d
 
 
