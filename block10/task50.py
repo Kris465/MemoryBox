5a4271af7adb3a977576e0915ee37a4fa5c07396
Kris465 <nataza7394@gmail.com>
2025-05-16 13:41:18 +0300

    ksusha

---
+++ block10/task50.py
@@ -0,0 +1,11 @@
+def ackermann(m, n):
+    if m == 0:
+        return n + 1
+    elif m > 0 and n == 0:
+        return ackermann(m - 1, 1)
+    else:
+        return ackermann(m - 1, ackermann(m, n - 1))
+
+
+result = ackermann(3, 1)
+print(f"A(3,1) = {result}")
