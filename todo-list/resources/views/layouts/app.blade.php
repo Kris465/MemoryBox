a19d4f0c922a2646dd71e1a4548cf42c598bb86f
Kris <oaisjdfoijsf@ijdfoasj>
2025-05-20 20:52:04 +0300

    app.blade.php is done

---
+++ todo-list/resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo List</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
+</head>
+<body class="bg-gray-100">
+    <div class=container mx-auto p-4>
+        @yield('content')
+    </div>
+</body>
+</html>
