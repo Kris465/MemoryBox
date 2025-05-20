8afb92a7d15c52ab20f83c79f48a53b670343b93
Kris <oaisjdfoijsf@ijdfoasj>
2025-05-20 20:42:29 +0300

    routes are added

--- todo-list/routes/web.php
+++ todo-list/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('tasks', TaskController::class);
+Route::get('/', [TaskController::class, 'index']);
