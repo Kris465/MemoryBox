d757eb83c7b03b01d95b026dfb355f77475e76dd
Kris465 <nataza7394@gmail.com>
2025-05-20 13:14:25 +0300

    Манираки

---
+++ block11/task21.py
@@ -0,0 +1,6 @@
+precipitations = [5, 0, 3, 2, 0, 4, 1, 0, 2, 3, 0, 1, 4, 0, 2, 3, 1, 0, 2, 4,
+                  0, 1, 3, 2, 0, 4, 1, 0, 2, 3, 0]
+
+total_precipitation = sum(precipitations)
+
+print("Общее количество осадков за январь:", total_precipitation)
