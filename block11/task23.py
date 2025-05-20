4a15e9c6695c171635e86e14e58458a57d724054
Kris465 <nataza7394@gmail.com>
2025-05-20 13:28:21 +0300

    fetisov

---
+++ block11/task23.py
@@ -0,0 +1,6 @@
+resistances = [10, 15, 20, 25, 30, 12, 18, 22, 28, 16,
+               14, 19, 23, 27, 13, 17, 21, 26, 11, 24]
+
+total_resistance = sum(resistances)
+
+print("Общее сопротивление цепи:", total_resistance)
