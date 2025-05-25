44dc304ac3881e7b62aaa5519e42289b9b4aa442
Kris <oaisjdfoijsf@ijdfoasj>
2025-05-25 18:07:58 +0300

    bot is updated from aiogram 2.x to aiogram 3.x

--- main.py
+++ main.py
@@ -1,27 +1,48 @@
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
+import asyncio
+import os
 
-from config import TOKEN
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
-bot = Bot(TOKEN)
-dp = Dispatcher(bot)
+from dotenv import find_dotenv, load_dotenv
+from loguru import logger
 
+load_dotenv(find_dotenv)
+TOKEN = os.getenv("BOT_TOKEN")
 
-@dp.message_handler(commands=['start'])
-async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nНапиши мне что-нибудь!")
-
-
-@dp.message_handler(commands=['help'])
-async def process_help_command(message: types.Message):
-    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+dp = Dispatcher()
 
 
-@dp.message_handler()
-async def echo_message(msg: types.Message):
-    await bot.send_message(msg.from_user.id, msg.text)
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
 
 
-if __name__ == '__main__':
-    executor.start_polling(dp)
+@dp.message()
+async def echo_handler(message: Message) -> None:
+    """
+    Handler will forward receive a message back to the sender
+
+    By default, message handler will handle all message types (like a text, photo, sticker etc.)
+    """
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.answer("Nice try!")
+
+
+async def main() -> None:
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logger.info("Бот запущен")
+    asyncio.run(main())
