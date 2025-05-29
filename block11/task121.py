ca463a9b73319c2ca0bb2903ce1fce4d7a144dd9
Kris465 <nataza7394@gmail.com>
2025-05-29 13:11:04 +0300

    vita

---
+++ block11/task121.py
@@ -0,0 +1,18 @@
+
+precipitations = [10, 20, 15, 20, 25, 25, 18, ...] 
+
+
+max_precipitation = None
+count_max_days = 0
+
+for precipitation in precipitations:
+    if max_precipitation is None or precipitation > max_precipitation:
+
+        max_precipitation = precipitation
+        count_max_days = 1
+    elif precipitation == max_precipitation:
+
+        count_max_days += 1
+
+print(f"Максимальное количество осадков за месяц: {max_precipitation}")
+print(f"Количество дней с этим количеством осадков: {count_max_days}")
