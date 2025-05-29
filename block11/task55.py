cb6952c64af77ca0965c87f8ea9cc5171461cc56
Kris465 <nataza7394@gmail.com>
2025-05-29 12:48:45 +0300

    some changes

--- block11/task55.py
+++ block11/task55.py
@@ -1,18 +1,15 @@
-# Ввод исходного массива (рост учеников)
 growths = list(map(int, input("Введите рост 25 учеников\
     через пробел: ").split()))
 if len(growths) != 25:
     print("Ошибка: необходимо ввести ровно 25 значений.")
     exit()
 
-# Ввод значений роста выбывших учеников
 lost_values = list(map(int, input("Введите рост выбывших учеников \
     через пробел (2 значения): ").split()))
 if len(lost_values) != 2:
     print("Ошибка: необходимо ввести два значения роста.")
     exit()
 
-# Удаляем по значениям роста
 remaining = growths.copy()
 for val in lost_values:
     if val in remaining:
