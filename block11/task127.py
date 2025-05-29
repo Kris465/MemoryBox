ce255586cdf6307be4ee0a72bbcd16b5c7151e78
Kris465 <nataza7394@gmail.com>
2025-05-29 13:24:17 +0300

    popov george

---
+++ block11/task127.py
@@ -0,0 +1,18 @@
+# Массив с весами людей
+weights = [/* ваши данные здесь */]
+
+# Инициализация переменных
+min_weight = None
+max_weight = None
+
+for weight in weights:
+    if min_weight is None or weight < min_weight:
+        min_weight = weight
+    if max_weight is None or weight > max_weight:
+        max_weight = weight
+
+# Проверка условия
+result = max_weight > 2 * min_weight
+
+print(f"Вес самого тяжелого превышает массу \
+    самого легкого более чем в 2 раза: {result}")
