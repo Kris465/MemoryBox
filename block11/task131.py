308c05e4a451e0b38704c8d33ddbfa1d278d041c
Kris465 <nataza7394@gmail.com>
2025-05-29 13:46:59 +0300

    vita

--- block11/task131.py
+++ block11/task131.py
@@ -1,20 +1,20 @@
-# Массив с данными о направлениях ветра за каждый день
-wind_directions = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 2, 4, ...]  # и так далее
 
-# Создаем словарь для подсчета количества дней по каждому направлению
+wind_directions = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 2, 4, ...]
+
+
 counts = {i:0 for i in range(1,9)}
 
-# Подсчитываем количество дней для каждого направления
+
 for direction in wind_directions:
     if direction in counts:
         counts[direction] += 1
 
-# Находим направление с минимальным числом дней
+
 min_days = min(counts.values())
 least_common_directions = [dir for dir, count in
                            counts.items() if count == min_days]
 
-# Выводим результат
+
 direction_names = {
     1: "северный",
     2: "южный",
---
+++ block11/task133.py
@@ -0,0 +1,15 @@
+
+speeds = [/* сюда вставьте 40 значений */]
+
+
+max_speed = float('-inf')
+second_max_speed = float('-inf')
+
+for speed in speeds:
+    if speed > max_speed:
+        second_max_speed = max_speed
+        max_speed = speed
+    elif speed > second_max_speed and speed != max_speed:
+        second_max_speed = speed
+
+print("Две самые быстрые скорости:", max_speed, second_max_speed)
