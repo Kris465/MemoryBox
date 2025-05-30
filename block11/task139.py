043516cff14d4fd102c13c93b114f700aad570f1
Kris465 <nataza7394@gmail.com>
2025-05-30 12:58:03 +0300

    Манираки

--- block11/task139.py
+++ block11/task139.py
@@ -5,4 +5,5 @@ min1_value = floors_copy[min1_index]
 floors_copy[min1_index] = float('inf')
 min2_index = floors_copy.index(min(floors_copy))
 min2_value = floors_copy[min2_index]
-print(f"Этажи с наименьшим количеством людей: {min1_index + 1} и {min2_index + 1}")
+print(f"Этажи с наименьшим количеством людей:\
+    {min1_index + 1} и {min2_index + 1}")
