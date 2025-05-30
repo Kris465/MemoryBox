dff2ef3bb4af3a6ec635bbc93f0b87e5dfa809d5
New_Lord_8951 <lukyanovivan83@gmail.com>
2025-05-30 21:42:02 +0300

    а я тебе не разрешал

--- Graphics.py
+++ Graphics.py
@@ -1,19 +1,19 @@
 import pygame
 import random
 
-# Инициализация Pygame
+# Ини
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((1000, 800))
 pygame.display.set_caption("Tetris")
 
 # Создаем тестовую сетку (1 — клетка закрашена, 0 — пустая)
-grid = [[random.randint(0, 1) for _ in range(10)] for _ in range(20)]
+grid = [[random.randint(-1, 1) for _ in range(100)] for _ in range(200)]
 
 def draw_grid(screen, grid):
     for y, row in enumerate(grid):
         for x, cell in enumerate(row):
             if cell:
-                pygame.draw.rect(screen, (255, 0, 0), (x * 30, y * 30, 30, 30))
+                pygame.draw.rect(screen, (200, 0, 0), (x * 40, y * 39, 30, 30))
 
 # Основной игровой цикл
 running = True
