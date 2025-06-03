9d6854c48dffff019e9f5640ed2d834a31c34514
Kris465 <nataza7394@gmail.com>
2025-06-03 13:02:07 +0300

    solomakhin

---
+++ block11/task166.py
@@ -0,0 +1,27 @@
+def bisect_insert(heights, new_height):
+    # Бинарный поиск места для вставки
+    left, right = 0, len(heights)
+    while left < right:
+        mid = (left + right) // 2
+        if heights[mid] > new_height:
+            left = mid + 1
+        else:
+            right = mid
+    heights.insert(left, new_height)
+
+
+def insert_by_values_efficient(heights, new_height1, new_height2):
+    # Вставляем сначала больший рост (чтобы не менять индексы)
+    if new_height1 > new_height2:
+        bisect_insert(heights, new_height1)
+        bisect_insert(heights, new_height2)
+    else:
+        bisect_insert(heights, new_height2)
+        bisect_insert(heights, new_height1)
+    return heights
+
+
+heights = [180, 175, 170, 165, 160]
+new_height1, new_height2 = 172, 168
+updated_heights = insert_by_values_efficient(heights, new_height1, new_height2)
+print(updated_heights)  # [180, 175, 172, 170, 168, 165, 160]
