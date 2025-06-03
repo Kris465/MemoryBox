6bf5d2b38faeac365933f2310e4674f24f79c729
Kris465 <nataza7394@gmail.com>
2025-06-03 12:52:28 +0300

    попов георгий

---
+++ block11/task163.py
@@ -0,0 +1,20 @@
+def insert_numbers(heights, num1, num2):
+    if not heights:
+        return [num2, num1]
+
+
+    max_height = heights[0]
+
+    heights.insert(0, num2)
+
+    index = 1
+    heights.insert(index + 1, num1)
+
+    return heights
+
+
+heights = [185, 180, 175, 170, 165]
+num1 = 177
+num2 = 190
+updated_heights = insert_numbers(heights, num1, num2)
+print(updated_heights)
