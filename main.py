7bad6351b40c5be196565fc0d9c6e6ea5572b81d
Kris465 <nataza7394@gmail.com>
2025-08-24 20:46:51 +0300

    draft locator

--- main.py
+++ main.py
@@ -11,13 +11,11 @@ def get_page_html(url):
         page.set_viewport_size({"width": 1200, 'height': 800})
 
         page.goto(url, timeout=60000)
-        page.wait_for_load_state('networkidle')
-        time.sleep(2)
+        page.wait_for_load_state('domcontentloaded')
+        page.wait_for_load_state('load')
 
-        print("Начало скролла")
-        scroll_to_bottom(page)
-        time.sleep(3)
-        
+        page.locator('//*[@id="main"]/div/div/div[4]/div[2]/div/div/div')
+
         html_content = page.content()
 
         with open('page.html', 'w', encoding='utf-8') as file:
@@ -25,31 +23,9 @@ def get_page_html(url):
 
         print('Верстка успешно сохранена в файл page.html')
         print(f'Размер HTML: {len(html_content)} символов')
-        
+
         browser.close()
         return html_content
-
-
-def scroll_to_bottom(page):
-    last_height = page.evaluate("() => document.documentElement.scrollHeight")
-    print(f"Начальная высота: {last_height}px")
-
-    scroll_attempts = 0
-    max_attempts = 20
-
-    while scroll_attempts < max_attempts:
-        page.evaluate("window.scrollTo(0, document.documentElement.scrollHeight)")
-        time.sleep(1.5)
-        
-        new_height = page.evaluate("() => document.documentElement.scrollHeight")
-        print(f"Высота после скролла #{scroll_attempts + 1}: {new_height}px")
-
-        if new_height == last_height:
-            print("Достигнута максимальная высота")
-            break
-
-        last_height = new_height
-        scroll_attempts += 1
 
 
 if __name__ == "__main__":
